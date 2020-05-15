5b1562aeb83997e912f447f7236577314be2d0d7
Jit Ray Chowdhury <jit.ray.c@gmail.com>
2020-05-15 22:09:12 +0000

    [#265] add transform of obstacle in recordreplay_planner_node

--- src/common/autoware_auto_tf2/include/autoware_auto_tf2/tf2_autoware_auto_msgs.hpp
+++ src/common/autoware_auto_tf2/include/autoware_auto_tf2/tf2_autoware_auto_msgs.hpp
@@ -19,11 +19,13 @@
 
 #include <tf2/convert.h>
 #include <tf2/time.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <autoware_auto_msgs/msg/bounding_box_array.hpp>
 #include <autoware_auto_msgs/msg/bounding_box.hpp>
 #include <geometry_msgs/msg/transform_stamped.hpp>
 #include <autoware_auto_msgs/msg/quaternion32.hpp>
 #include <geometry_msgs/msg/point32.hpp>
+#include <kdl/frames.hpp>
 #include <common/types.hpp>
 #include <string>
 
