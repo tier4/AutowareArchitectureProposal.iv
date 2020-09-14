78371c68fb718811baf48336e3a08abdb9afb42f
Yukihiro Saito <yukky.saito@gmail.com>
2020-09-14 14:05:41 +0900

    change alpha (#903)
    Signed-off-by: Yukihiro Saito <yukky.saito@gmail.com>

--- perception/traffic_light_recognition/traffic_light_map_based_detector/src/node.cpp
+++ perception/traffic_light_recognition/traffic_light_map_based_detector/src/node.cpp
@@ -415,7 +415,7 @@ void MapBasedDetector::publishVisibleTrafficLights(
     marker.points.push_back(point);
 
     marker.lifetime = rclcpp::Duration::from_seconds(0.2);
-    marker.color.a = 1.0;  // Don't forget to set the alpha!
+    marker.color.a = 0.999;  // Don't forget to set the alpha!
     marker.color.r = 0.0;
     marker.color.g = 1.0;
     marker.color.b = 0.0;
