665a0f8e41320392631b359a843291aad66f0cf9
tkimura4 <tomoya.kimura@tier4.jp>
2020-09-24 18:49:18 +0900

    add pacmod debug publisher (#921)
    * add pacmod debug publisher
    
    * add dependency
    
    * fix bug
    
    * remove debug_publisher from launch
    
    * fix bug
    
    * add license and apply format
    
    * fix typo

---
+++ vehicle/as/include/pacmod_additional_debug_publisher/pacmod_additional_debug_publisher_node.hpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2020 Tier IV, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+#include <can_msgs/Frame.h>
+#include <ros/ros.h>
+#include <std_msgs/Float32MultiArray.h>
+
+class PacmodAdditionalDebugPublisherNode
+{
+private:
+  ros::NodeHandle nh_, pnh_;
+  ros::Publisher pub_;
+  ros::Subscriber sub_;
+  std_msgs::Float32MultiArray debug_value_;
+  void canTxCallback(const can_msgs::FrameConstPtr & msg);
+
+public:
+  PacmodAdditionalDebugPublisherNode();
+  ~PacmodAdditionalDebugPublisherNode(){};
+};
