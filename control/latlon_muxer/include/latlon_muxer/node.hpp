57c5f5d4391ac4e6aac5e1df80eea9a826a234ae
Esteve Fernandez <esteve@apache.org>
2020-10-07 17:57:30 +0200

    Reformat code
    Signed-off-by: Esteve Fernandez <esteve@apache.org>

--- control/latlon_muxer/include/latlon_muxer/node.hpp
+++ control/latlon_muxer/include/latlon_muxer/node.hpp
@@ -1,26 +1,25 @@
-/*
- * Copyright 2015-2020 Autoware Foundation. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2015-2020 Autoware Foundation. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-#ifndef CONTROL_LATLON_MUXER_INCLUDE_LATLON_MUXER_NODE_HPP_
-#define CONTROL_LATLON_MUXER_INCLUDE_LATLON_MUXER_NODE_HPP_
-
-#include <memory>
+#ifndef LATLON_MUXER__NODE_HPP_
+#define LATLON_MUXER__NODE_HPP_
 
 #include <autoware_control_msgs/msg/control_command_stamped.hpp>
 #include <rclcpp/rclcpp.hpp>
+
+#include <memory>
+#include <string>
 
 class LatLonMuxer : public rclcpp::Node
 {
@@ -33,11 +32,13 @@ private:
   void publishCmd();
 
   rclcpp::Publisher<autoware_control_msgs::msg::ControlCommandStamped>::SharedPtr control_cmd_pub_;
-  rclcpp::Subscription<autoware_control_msgs::msg::ControlCommandStamped>::SharedPtr lat_control_cmd_sub_;
-  rclcpp::Subscription<autoware_control_msgs::msg::ControlCommandStamped>::SharedPtr lon_control_cmd_sub_;
+  rclcpp::Subscription<autoware_control_msgs::msg::ControlCommandStamped>::SharedPtr
+    lat_control_cmd_sub_;
+  rclcpp::Subscription<autoware_control_msgs::msg::ControlCommandStamped>::SharedPtr
+    lon_control_cmd_sub_;
 
   std::shared_ptr<autoware_control_msgs::msg::ControlCommandStamped> lat_cmd_;
   std::shared_ptr<autoware_control_msgs::msg::ControlCommandStamped> lon_cmd_;
 };
 
-#endif  // CONTROL_LATLON_MUXER_INCLUDE_LATLON_MUXER_NODE_HPP_
+#endif  // LATLON_MUXER__NODE_HPP_
--- control/latlon_muxer/src/node.cpp
+++ control/latlon_muxer/src/node.cpp
@@ -1,32 +1,39 @@
-/*
- * Copyright 2015-2012 Autoware Foundation. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2015-2020 Autoware Foundation. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include "latlon_muxer/node.hpp"
 
 #include <rclcpp/time.hpp>
 
+#include <string>
+#include <memory>
+
 LatLonMuxer::LatLonMuxer(const std::string & node_name, const rclcpp::NodeOptions & options)
-  : rclcpp::Node(node_name, options)
+: rclcpp::Node(node_name, options)
 {
   control_cmd_pub_ =
-    create_publisher<autoware_control_msgs::msg::ControlCommandStamped>("output/control_cmd", rclcpp::QoS{1}.transient_local());
+    create_publisher<autoware_control_msgs::msg::ControlCommandStamped>(
+    "output/control_cmd",
+    rclcpp::QoS{1}.transient_local());
   lat_control_cmd_sub_ =
-    create_subscription<autoware_control_msgs::msg::ControlCommandStamped>("input/lateral/control_cmd", rclcpp::QoS{1}, std::bind(&LatLonMuxer::latCtrlCmdCallback, this, std::placeholders::_1));
+    create_subscription<autoware_control_msgs::msg::ControlCommandStamped>(
+    "input/lateral/control_cmd", rclcpp::QoS{1},
+    std::bind(&LatLonMuxer::latCtrlCmdCallback, this, std::placeholders::_1));
   lon_control_cmd_sub_ =
-    create_subscription<autoware_control_msgs::msg::ControlCommandStamped>("input/longitudinal/control_cmd", rclcpp::QoS{1}, std::bind(&LatLonMuxer::lonCtrlCmdCallback, this, std::placeholders::_1));
+    create_subscription<autoware_control_msgs::msg::ControlCommandStamped>(
+    "input/longitudinal/control_cmd", rclcpp::QoS{1},
+    std::bind(&LatLonMuxer::lonCtrlCmdCallback, this, std::placeholders::_1));
 }
 
 void LatLonMuxer::publishCmd()
