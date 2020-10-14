c2c8a19c8ee25ff35fb717fdaa3102d0aa0e7b9e
Esteve Fernandez <esteve@apache.org>
2020-10-14 11:41:08 +0200

    Port latlon_muxer to ROS 2 (#6)
    * Port CMakeLists.txt and package.xml
    
    Signed-off-by: Esteve Fernandez <esteve@apache.org>
    
    * First cut at a port
    
    Signed-off-by: Esteve Fernandez <esteve@apache.org>
    
    * Port rest of the code
    
    Signed-off-by: Esteve Fernandez <esteve@apache.org>
    
    * Reformat code
    
    Signed-off-by: Esteve Fernandez <esteve@apache.org>
    
    * Removed COLCON_IGNORE por latlon_muxer
    
    Signed-off-by: Esteve Fernandez <esteve@apache.org>
    
    * Reenable stricter compile options
    
    Signed-off-by: Esteve Fernandez <esteve@apache.org>
    
    * Treat warnings as errors
    
    Signed-off-by: Esteve Fernandez <esteve@apache.org>
    
    * Use ament_auto
    
    Signed-off-by: Esteve Fernandez <esteve@apache.org>
    
    * Convert node to a component
    
    Signed-off-by: Esteve Fernandez <esteve@apache.org>
    
    * Made library shared

--- control/latlon_muxer/src/node.cpp
+++ control/latlon_muxer/src/node.cpp
@@ -1,43 +1,40 @@
-/*
- *  Copyright (c) 2017, Tier IV, Inc.
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- *  * Neither the name of Autoware nor the names of its
- *    contributors may be used to endorse or promote products derived from
- *    this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+// Copyright 2015-2020 Autoware Foundation. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include "latlon_muxer/node.hpp"
 
-LatLonMuxer::LatLonMuxer() : nh_(""), pnh_("~")
+#include <rclcpp_components/register_node_macro.hpp>
+#include <rclcpp/time.hpp>
+
+#include <string>
+#include <memory>
+
+LatLonMuxer::LatLonMuxer(const rclcpp::NodeOptions & node_options)
+: rclcpp::Node("latlon_muxer", node_options)
 {
   control_cmd_pub_ =
-    pnh_.advertise<autoware_control_msgs::ControlCommandStamped>("output/control_cmd", 1, true);
+    create_publisher<autoware_control_msgs::msg::ControlCommandStamped>(
+    "output/control_cmd",
+    rclcpp::QoS{1}.transient_local());
   lat_control_cmd_sub_ =
-    pnh_.subscribe("input/lateral/control_cmd", 1, &LatLonMuxer::latCtrlCmdCallback, this);
+    create_subscription<autoware_control_msgs::msg::ControlCommandStamped>(
+    "input/lateral/control_cmd", rclcpp::QoS{1},
+    std::bind(&LatLonMuxer::latCtrlCmdCallback, this, std::placeholders::_1));
   lon_control_cmd_sub_ =
-    pnh_.subscribe("input/longitudinal/control_cmd", 1, &LatLonMuxer::lonCtrlCmdCallback, this);
+    create_subscription<autoware_control_msgs::msg::ControlCommandStamped>(
+    "input/longitudinal/control_cmd", rclcpp::QoS{1},
+    std::bind(&LatLonMuxer::lonCtrlCmdCallback, this, std::placeholders::_1));
 }
 
 void LatLonMuxer::publishCmd()
@@ -46,27 +43,29 @@ void LatLonMuxer::publishCmd()
     return;
   }
 
-  autoware_control_msgs::ControlCommandStamped out;
-  out.header.stamp = ros::Time::now();
+  autoware_control_msgs::msg::ControlCommandStamped out;
+  out.header.stamp = rclcpp::Node::now();
   out.header.frame_id = "base_link";
   out.control.steering_angle = lat_cmd_->control.steering_angle;
   out.control.steering_angle_velocity = lat_cmd_->control.steering_angle_velocity;
   out.control.velocity = lon_cmd_->control.velocity;
   out.control.acceleration = lon_cmd_->control.acceleration;
 
-  control_cmd_pub_.publish(out);
+  control_cmd_pub_->publish(out);
 }
 
 void LatLonMuxer::latCtrlCmdCallback(
-  const autoware_control_msgs::ControlCommandStamped::ConstPtr input_msg)
+  const autoware_control_msgs::msg::ControlCommandStamped::SharedPtr input_msg)
 {
-  lat_cmd_ = std::make_shared<autoware_control_msgs::ControlCommandStamped>(*input_msg);
+  lat_cmd_ = std::make_shared<autoware_control_msgs::msg::ControlCommandStamped>(*input_msg);
   publishCmd();
 }
 
 void LatLonMuxer::lonCtrlCmdCallback(
-  const autoware_control_msgs::ControlCommandStamped::ConstPtr input_msg)
+  const autoware_control_msgs::msg::ControlCommandStamped::SharedPtr input_msg)
 {
-  lon_cmd_ = std::make_shared<autoware_control_msgs::ControlCommandStamped>(*input_msg);
+  lon_cmd_ = std::make_shared<autoware_control_msgs::msg::ControlCommandStamped>(*input_msg);
   publishCmd();
 }
+
+RCLCPP_COMPONENTS_REGISTER_NODE(LatLonMuxer)
