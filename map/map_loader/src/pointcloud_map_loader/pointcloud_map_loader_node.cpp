e70fb61105062033eea891e1e695954a02d7c95e
mitsudome-r <ryohsuke.mitsudome@tier4.jp>
2020-10-19 15:10:58 +0900

    port map_loader to ROS2
    Signed-off-by: mitsudome-r <ryohsuke.mitsudome@tier4.jp>

--- map/map_loader/src/pointcloud_map_loader/pointcloud_map_loader_node.cpp
+++ map/map_loader/src/pointcloud_map_loader/pointcloud_map_loader_node.cpp
@@ -36,36 +36,39 @@
 #include <pcl_conversions/pcl_conversions.h>
 
 PointCloudMapLoaderNode::PointCloudMapLoaderNode(const std::vector<std::string> & pcd_paths)
+: Node("poincloud_map_loader")
 {
+  rclcpp::QoS durable_qos{1};
+  durable_qos.transient_local();
   pub_pointcloud_map_ =
-    private_nh_.advertise<sensor_msgs::PointCloud2>("output/pointcloud_map", 1, true);
+    this->create_publisher<sensor_msgs::msg::PointCloud2>("output/pointcloud_map", durable_qos);
 
   const auto pcd = loadPCDFiles(pcd_paths);
 
   if (pcd.width == 0) {
-    ROS_ERROR("No PCD was loaded: pcd_paths.size() = %zu", pcd_paths.size());
+    RCLCPP_ERROR(get_logger(), "No PCD was loaded: pcd_paths.size() = %zu", pcd_paths.size());
     return;
   }
 
-  pub_pointcloud_map_.publish(pcd);
+  pub_pointcloud_map_->publish(pcd);
 }
 
-sensor_msgs::PointCloud2 PointCloudMapLoaderNode::loadPCDFiles(
+sensor_msgs::msg::PointCloud2 PointCloudMapLoaderNode::loadPCDFiles(
   const std::vector<std::string> & pcd_paths)
 {
-  sensor_msgs::PointCloud2 whole_pcd{};
+  sensor_msgs::msg::PointCloud2 whole_pcd{};
 
-  sensor_msgs::PointCloud2 partial_pcd;
+  sensor_msgs::msg::PointCloud2 partial_pcd;
   for (const auto & path : pcd_paths) {
     if (pcl::io::loadPCDFile(path, partial_pcd) == -1) {
-      ROS_ERROR_STREAM("PCD load failed: " << path);
+      RCLCPP_ERROR_STREAM(get_logger(), "PCD load failed: " << path);
     }
 
     if (whole_pcd.width == 0) {
       whole_pcd = partial_pcd;
     } else {
       whole_pcd.width += partial_pcd.width;
-      whole_pcd.row_step += partial_pcd.row_step;
+      whole_pcd.row_step = partial_pcd.row_step;
       whole_pcd.data.reserve(whole_pcd.data.size() + partial_pcd.data.size());
       whole_pcd.data.insert(whole_pcd.data.end(), partial_pcd.data.begin(), partial_pcd.data.end());
     }
