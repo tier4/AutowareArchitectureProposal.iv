265beeca8c28967015f0b9c996cf33881119886e
Jilada Eccleston <jilada.eccleston@tier4.jp>
2020-10-28 12:20:41 +0900

    First pass  - Remove ROS references: dynamic_configuration  - Can compile

--- system/util/dummy_diag_publisher/include/dummy_diag_publisher/dummy_diag_publisher_node.h
+++ system/util/dummy_diag_publisher/include/dummy_diag_publisher/dummy_diag_publisher_node.h
@@ -14,29 +14,30 @@
  * limitations under the License.
  */
 
-#pragma once
+#ifndef DUMMY_DIAG_PUBLISHER_DUMMY_DIAG_PUBLISHER_H_
+#define DUMMY_DIAG_PUBLISHER_DUMMY_DIAG_PUBLISHER_H_ 
 
 #include <string>
 
-#include <ros/ros.h>
+#include <rclcpp/rclcpp.hpp>
 
-#include <dummy_diag_publisher/DummyDiagPublisherConfig.h>
-#include <dynamic_reconfigure/server.h>
+// #include <dummy_diag_publisher/DummyDiagPublisherConfig.h>
+// #include <dynamic_reconfigure/server.h>
 
-#include <diagnostic_updater/diagnostic_updater.h>
+#include <diagnostic_updater/diagnostic_updater.hpp>
 
 struct DiagConfig
 {
   DiagConfig() = default;
-  explicit DiagConfig(XmlRpc::XmlRpcValue value)
-  : name(static_cast<std::string>(value["name"])),
-    hardware_id(static_cast<std::string>(value["hardware_id"])),
-    msg_ok(static_cast<std::string>(value["msg_ok"])),
-    msg_warn(static_cast<std::string>(value["msg_warn"])),
-    msg_error(static_cast<std::string>(value["msg_error"])),
-    msg_stale(static_cast<std::string>(value["msg_stale"]))
-  {
-  }
+  // explicit DiagConfig(XmlRpc::XmlRpcValue value)
+  // : name(static_cast<std::string>(value["name"])),
+  //   hardware_id(static_cast<std::string>(value["hardware_id"])),
+  //   msg_ok(static_cast<std::string>(value["msg_ok"])),
+  //   msg_warn(static_cast<std::string>(value["msg_warn"])),
+  //   msg_error(static_cast<std::string>(value["msg_error"])),
+  //   msg_stale(static_cast<std::string>(value["msg_stale"]))
+  // {
+  // }
 
   std::string name;
   std::string hardware_id;
@@ -46,26 +47,28 @@ struct DiagConfig
   std::string msg_stale;
 };
 
-class DummyDiagPublisherNode
+// Create enum with ok and warn types
+
+class DummyDiagPublisherNode : public rclcpp::Node
 {
 public:
   DummyDiagPublisherNode();
 
 private:
   // NodeHandle
-  ros::NodeHandle nh_{""};
-  ros::NodeHandle private_nh_{"~"};
+  // ros::NodeHandle nh_{""};
+  // ros::NodeHandle private_nh_{"~"};
 
   // Parameter
   double update_rate_;
   DiagConfig diag_config_;
 
   // Dynamic Reconfigure
-  void onConfig(
-    const dummy_diag_publisher::DummyDiagPublisherConfig & config, const uint32_t level);
+  // void onConfig(
+  //   const dummy_diag_publisher::DummyDiagPublisherConfig & config, const uint32_t level);
 
-  dynamic_reconfigure::Server<dummy_diag_publisher::DummyDiagPublisherConfig> dynamic_reconfigure_;
-  dummy_diag_publisher::DummyDiagPublisherConfig config_;
+  // dynamic_reconfigure::Server<dummy_diag_publisher::DummyDiagPublisherConfig> dynamic_reconfigure_;
+  // dummy_diag_publisher::DummyDiagPublisherConfig config_;
 
   // Diagnostic Updater
   diagnostic_updater::Updater updater_;
@@ -73,6 +76,8 @@ private:
   void produceDiagnostics(diagnostic_updater::DiagnosticStatusWrapper & stat);
 
   // Timer
-  void onTimer(const ros::TimerEvent & event);
-  ros::Timer timer_;
+  void onTimer();
+  // ros::Timer timer_;
 };
+
+#endif
--- system/util/dummy_diag_publisher/src/dummy_diag_publisher_node/dummy_diag_publisher_node.cpp
+++ system/util/dummy_diag_publisher/src/dummy_diag_publisher_node/dummy_diag_publisher_node.cpp
@@ -14,75 +14,78 @@
  * limitations under the License.
  */
 
-#include <dummy_diag_publisher/dummy_diag_publisher_node.h>
+#include "dummy_diag_publisher/dummy_diag_publisher_node.h"
 
 #include <boost/bind.hpp>
 
+#define FMT_HEADER_ONLY
 #include <fmt/format.h>
 
 namespace
 {
 template <class Config>
-Config getConfig(const ros::NodeHandle & nh, const std::string & config_name)
+Config getConfig(const std::string & config_name)
 {
-  XmlRpc::XmlRpcValue xml;
-  if (!nh.getParam(config_name, xml)) {
-    throw std::runtime_error(fmt::format("no parameter found: {}", config_name));
-  }
+  Config config;
+  // XmlRpc::XmlRpcValue xml;
+  // if (!nh.getParam(config_name, xml)) {
+  //   throw std::runtime_error(fmt::format("no parameter found: {}", config_name));
+  // }
 
-  return Config{xml};
+  return config;
 }
 }  // namespace
 
-void DummyDiagPublisherNode::onConfig(
-  const dummy_diag_publisher::DummyDiagPublisherConfig & config, const uint32_t level)
-{
-  config_ = config;
-}
+// void DummyDiagPublisherNode::onConfig(
+//   const dummy_diag_publisher::DummyDiagPublisherConfig & config, const uint32_t level)
+// {
+//   // config_ = config;
+// }
 
 void DummyDiagPublisherNode::produceDiagnostics(diagnostic_updater::DiagnosticStatusWrapper & stat)
 {
-  diagnostic_msgs::DiagnosticStatus status;
+  diagnostic_msgs::msg::DiagnosticStatus status;
 
-  if (config_.status == dummy_diag_publisher::DummyDiagPublisher_OK) {
-    status.level = diagnostic_msgs::DiagnosticStatus::OK;
-    status.message = diag_config_.msg_ok;
-  } else if (config_.status == dummy_diag_publisher::DummyDiagPublisher_WARN) {
-    status.level = diagnostic_msgs::DiagnosticStatus::WARN;
-    status.message = diag_config_.msg_warn;
-  } else if (config_.status == dummy_diag_publisher::DummyDiagPublisher_ERROR) {
-    status.level = diagnostic_msgs::DiagnosticStatus::ERROR;
-    status.message = diag_config_.msg_error;
-  } else if (config_.status == dummy_diag_publisher::DummyDiagPublisher_STALE) {
-    status.level = diagnostic_msgs::DiagnosticStatus::STALE;
-    status.message = diag_config_.msg_stale;
-  } else {
-    throw std::runtime_error("invalid status");
-  }
+  // if (config_.status == dummy_diag_publisher::DummyDiagPublisher_OK) {
+  //   status.level = diagnostic_msgs::DiagnosticStatus::OK;
+  //   status.message = diag_config_.msg_ok;
+  // } else if (config_.status == dummy_diag_publisher::DummyDiagPublisher_WARN) {
+  //   status.level = diagnostic_msgs::DiagnosticStatus::WARN;
+  //   status.message = diag_config_.msg_warn;
+  // } else if (config_.status == dummy_diag_publisher::DummyDiagPublisher_ERROR) {
+  //   status.level = diagnostic_msgs::DiagnosticStatus::ERROR;
+  //   status.message = diag_config_.msg_error;
+  // } else if (config_.status == dummy_diag_publisher::DummyDiagPublisher_STALE) {
+  //   status.level = diagnostic_msgs::DiagnosticStatus::STALE;
+  //   status.message = diag_config_.msg_stale;
+  // } else {
+  //   throw std::runtime_error("invalid status");
+  // }
 
-  stat.summary(status.level, status.message);
+  // stat.summary(status.level, status.message);
 }
 
-void DummyDiagPublisherNode::onTimer(const ros::TimerEvent & event)
+void DummyDiagPublisherNode::onTimer()
 {
-  if (config_.is_active) {
-    updater_.force_update();
-  }
+  // if (config_.is_active) {
+  //   updater_.force_update();
+  // }
 }
 
-DummyDiagPublisherNode::DummyDiagPublisherNode()
+DummyDiagPublisherNode::DummyDiagPublisherNode() : Node("dummy_diag_publisher"), updater_(this)
 {
   // Parameter
-  private_nh_.param("update_rate", update_rate_, 10.0);
-  diag_config_ = getConfig<DiagConfig>(private_nh_, "diag_config");
+  // private_nh_.param("update_rate", update_rate_, 10.0);
+  // diag_config_ = getConfig<DiagConfig>(private_nh_, "diag_config");
 
   // Dynamic Reconfigure
-  dynamic_reconfigure_.setCallback(boost::bind(&DummyDiagPublisherNode::onConfig, this, _1, _2));
+  // Change to std::bind?
+  // dynamic_reconfigure_.setCallback(boost::bind(&DummyDiagPublisherNode::onConfig, this, _1, _2));
 
   // Diagnostic Updater
-  updater_.setHardwareID(diag_config_.hardware_id);
-  updater_.add(diag_config_.name, this, &DummyDiagPublisherNode::produceDiagnostics);
+  // updater_.setHardwareID(diag_config_.hardware_id);
+  // updater_.add(diag_config_.name, this, &DummyDiagPublisherNode::produceDiagnostics);
 
   // Timer
-  timer_ = private_nh_.createTimer(ros::Rate(update_rate_), &DummyDiagPublisherNode::onTimer, this);
+  // timer_ = private_nh_.createTimer(ros::Rate(update_rate_), &DummyDiagPublisherNode::onTimer, this);
 }
