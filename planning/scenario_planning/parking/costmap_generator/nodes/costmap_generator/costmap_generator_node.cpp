c9a987a18fec96178beb732a22ec3c7cf44008cf
mitsudome-r <ryohsuke.mitsudome@tier4.jp>
2020-11-13 19:46:34 +0900

    fix parameter declarations
    Signed-off-by: mitsudome-r <ryohsuke.mitsudome@tier4.jp>

--- planning/scenario_planning/parking/costmap_generator/nodes/costmap_generator/costmap_generator_node.cpp
+++ planning/scenario_planning/parking/costmap_generator/nodes/costmap_generator/costmap_generator_node.cpp
@@ -90,24 +90,24 @@ std::vector<geometry_msgs::msg::Point> poly2vector(const geometry_msgs::msg::Pol
 CostmapGenerator::CostmapGenerator() : Node("costmap_generator")
 {
   // Parameters
-  this->declare_parameter<std::string>("costmap_frame", "map");
-  this->declare_parameter<std::string>("vehicle_frame", "base_link");
-  this->declare_parameter<std::string>("map_frame", "map");
-  this->declare_parameter<double>("update_rate", 10.0);
-  this->declare_parameter<double>("grid_min_value", 0.0);
-  this->declare_parameter<double>("grid_max_value", 1.0);
-  this->declare_parameter<double>("grid_resolution", 0.2);
-  this->declare_parameter<double>("grid_length_x", 50);
-  this->declare_parameter<double>("grid_length_y", 30);
-  this->declare_parameter<double>("grid_position_x", 20);
-  this->declare_parameter<double>("grid_position_y", 0);
-  this->declare_parameter<double>("maximum_lidar_height_thres", 0.3);
-  this->declare_parameter<double>("minimum_lidar_height_thres", -2.2);
-  this->declare_parameter<bool>("use_objects", true);
-  this->declare_parameter<bool>("use_points", true);
-  this->declare_parameter<bool>("use_wayarea", true);
-  this->declare_parameter<double>("expand_polygon_size", 1.0);
-  this->declare_parameter<int>("size_of_expansion_kernel", 9);
+  costmap_frame_ = this->declare_parameter<std::string>("costmap_frame", "map");
+  vehicle_frame_ = this->declare_parameter<std::string>("vehicle_frame", "base_link");
+  map_frame_ = this->declare_parameter<std::string>("map_frame", "map");
+  update_rate_ = this->declare_parameter<double>("update_rate", 10.0);
+  grid_min_value_ = this->declare_parameter<double>("grid_min_value", 0.0);
+  grid_max_value_ = this->declare_parameter<double>("grid_max_value", 1.0);
+  grid_resolution_ = this->declare_parameter<double>("grid_resolution", 0.2);
+  grid_length_x_ = this->declare_parameter<double>("grid_length_x", 50);
+  grid_length_y_ = this->declare_parameter<double>("grid_length_y", 30);
+  grid_position_x_ = this->declare_parameter<double>("grid_position_x", 20);
+  grid_position_y_ = this->declare_parameter<double>("grid_position_y", 0);
+  maximum_lidar_height_thres_ = this->declare_parameter<double>("maximum_lidar_height_thres", 0.3);
+  minimum_lidar_height_thres_ = this->declare_parameter<double>("minimum_lidar_height_thres", -2.2);
+  use_objects_ = this->declare_parameter<bool>("use_objects", true);
+  use_points_ = this->declare_parameter<bool>("use_points", true);
+  use_wayarea_ = this->declare_parameter<bool>("use_wayarea", true);
+  expand_polygon_size_ = this->declare_parameter<double>("expand_polygon_size", 1.0);
+  size_of_expansion_kernel_ = this->declare_parameter<int>("size_of_expansion_kernel", 9);
 
   // Subscribers
   using std::placeholders::_1;
