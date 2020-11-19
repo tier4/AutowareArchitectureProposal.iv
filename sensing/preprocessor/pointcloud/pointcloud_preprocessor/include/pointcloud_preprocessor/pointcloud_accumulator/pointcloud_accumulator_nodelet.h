e6ca877c6badd85c58fd00fec114722a1b30ee67
Takamasa Horibe <horibe.takamasa@gmail.com>
2020-11-20 01:08:27 +0900

    port pointcloud accumulator
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>

--- sensing/preprocessor/pointcloud/pointcloud_preprocessor/include/pointcloud_preprocessor/pointcloud_accumulator/pointcloud_accumulator_nodelet.h
+++ sensing/preprocessor/pointcloud/pointcloud_preprocessor/include/pointcloud_preprocessor/pointcloud_accumulator/pointcloud_accumulator_nodelet.h
@@ -16,31 +16,26 @@
 #pragma once
 
 #include <boost/circular_buffer.hpp>
-#include "pointcloud_preprocessor/PointcloudAccumulatorConfig.h"
 #include "pointcloud_preprocessor/filter.h"
 
 namespace pointcloud_preprocessor
 {
-class PointcloudAccumulatorNodelet : public pointcloud_preprocessor::Filter
+class PointcloudAccumulatorComponent : public pointcloud_preprocessor::Filter
 {
 protected:
-  boost::shared_ptr<
-    dynamic_reconfigure::Server<pointcloud_preprocessor::PointcloudAccumulatorConfig> >
-    srv_;
   virtual void filter(
-    const PointCloud2::ConstPtr & input, const IndicesPtr & indices, PointCloud2 & output);
-  virtual void subscribe();
-  virtual void unsubscribe();
+    const PointCloud2ConstPtr & input, const IndicesPtr & indices, PointCloud2 & output);
 
-  bool child_init(ros::NodeHandle & nh, bool & has_service);
-  void config_callback(
-    pointcloud_preprocessor::PointcloudAccumulatorConfig & config, uint32_t level);
+  // TODO(Horibe)
+  // void config_callback(
+  //   pointcloud_preprocessor::PointcloudAccumulatorConfig & config, uint32_t level);
 
 private:
   double accumulation_time_sec_;
-  boost::circular_buffer<PointCloud2::ConstPtr> pointcloud_buffer_;
+  boost::circular_buffer<PointCloud2ConstPtr> pointcloud_buffer_;
 
 public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  PointcloudAccumulatorComponent(const rclcpp::NodeOptions &options);
 };
 }  // namespace pointcloud_preprocessor
--- sensing/preprocessor/pointcloud/pointcloud_preprocessor/src/pointcloud_accumulator/pointcloud_accumulator_nodelet.cpp
+++ sensing/preprocessor/pointcloud/pointcloud_preprocessor/src/pointcloud_accumulator/pointcloud_accumulator_nodelet.cpp
@@ -15,32 +15,24 @@
  */
 
 #include "pointcloud_preprocessor/pointcloud_accumulator/pointcloud_accumulator_nodelet.h"
-#include <pluginlib/class_list_macros.h>
 
 namespace pointcloud_preprocessor
 {
-bool PointcloudAccumulatorNodelet::child_init(ros::NodeHandle & nh, bool & has_service)
+PointcloudAccumulatorComponent::PointcloudAccumulatorComponent(const rclcpp::NodeOptions & options)
+: Filter("PointcloudAccumulator", options)
 {
-  // Enable the dynamic reconfigure service
-  has_service = true;
-  srv_ = boost::make_shared<
-    dynamic_reconfigure::Server<pointcloud_preprocessor::PointcloudAccumulatorConfig> >(nh);
-  dynamic_reconfigure::Server<pointcloud_preprocessor::PointcloudAccumulatorConfig>::CallbackType
-    f = boost::bind(&PointcloudAccumulatorNodelet::config_callback, this, _1, _2);
-  srv_->setCallback(f);
-  return (true);
 }
 
-void PointcloudAccumulatorNodelet::filter(
-  const PointCloud2::ConstPtr & input, const IndicesPtr & indices, PointCloud2 & output)
+void PointcloudAccumulatorComponent::filter(
+  const PointCloud2ConstPtr & input, const IndicesPtr & indices, PointCloud2 & output)
 {
   boost::mutex::scoped_lock lock(mutex_);
   pointcloud_buffer_.push_front(input);
-  ros::Time last_time = input->header.stamp;
+  rclcpp::Time last_time = input->header.stamp;
   pcl::PointCloud<pcl::PointXYZ> pcl_input;
   pcl::PointCloud<pcl::PointXYZ> pcl_output;
   for (size_t i = 0; i < pointcloud_buffer_.size(); i++) {
-    if (accumulation_time_sec_ < (last_time - pointcloud_buffer_.at(i)->header.stamp).toSec())
+    if (accumulation_time_sec_ < (last_time - pointcloud_buffer_.at(i)->header.stamp).seconds())
       break;
     pcl::fromROSMsg(*pointcloud_buffer_.at(i), pcl_input);
     pcl_output += pcl_input;
@@ -49,41 +41,38 @@ void PointcloudAccumulatorNodelet::filter(
   output.header = input->header;
 }
 
-void PointcloudAccumulatorNodelet::subscribe() { Filter::subscribe(); }
+// void PointcloudAccumulatorComponent::config_callback(
+//   pointcloud_preprocessor::PointcloudAccumulatorConfig & config, uint32_t level)
+// {
+//   boost::mutex::scoped_lock lock(mutex_);
 
-void PointcloudAccumulatorNodelet::unsubscribe() { Filter::unsubscribe(); }
-
-void PointcloudAccumulatorNodelet::config_callback(
-  pointcloud_preprocessor::PointcloudAccumulatorConfig & config, uint32_t level)
-{
-  boost::mutex::scoped_lock lock(mutex_);
-
-  if (accumulation_time_sec_ != config.accumulation_time_sec) {
-    accumulation_time_sec_ = config.accumulation_time_sec;
-    NODELET_DEBUG(
-      "[%s::config_callback] Setting new accumulation time to: %f.", getName().c_str(),
-      config.accumulation_time_sec);
-  }
-  if (pointcloud_buffer_.size() != (size_t)config.pointcloud_buffer_size) {
-    NODELET_DEBUG(
-      "[%s::config_callback] Setting new buffer size to: %d.", getName().c_str(),
-      config.pointcloud_buffer_size);
-  }
-  pointcloud_buffer_.set_capacity((size_t)config.pointcloud_buffer_size);
-  // ---[ These really shouldn't be here, and as soon as dynamic_reconfigure improves, we'll remove them and inherit
-  // from Filter
-  if (tf_input_frame_ != config.input_frame) {
-    tf_input_frame_ = config.input_frame;
-    NODELET_DEBUG("[config_callback] Setting the input TF frame to: %s.", tf_input_frame_.c_str());
-  }
-  if (tf_output_frame_ != config.output_frame) {
-    tf_output_frame_ = config.output_frame;
-    NODELET_DEBUG(
-      "[config_callback] Setting the output TF frame to: %s.", tf_output_frame_.c_str());
-  }
-  // ]---
-}
+//   if (accumulation_time_sec_ != config.accumulation_time_sec) {
+//     accumulation_time_sec_ = config.accumulation_time_sec;
+//     NODELET_DEBUG(
+//       "[%s::config_callback] Setting new accumulation time to: %f.", getName().c_str(),
+//       config.accumulation_time_sec);
+//   }
+//   if (pointcloud_buffer_.size() != (size_t)config.pointcloud_buffer_size) {
+//     NODELET_DEBUG(
+//       "[%s::config_callback] Setting new buffer size to: %d.", getName().c_str(),
+//       config.pointcloud_buffer_size);
+//   }
+//   pointcloud_buffer_.set_capacity((size_t)config.pointcloud_buffer_size);
+//   // ---[ These really shouldn't be here, and as soon as dynamic_reconfigure improves, we'll remove them and inherit
+//   // from Filter
+//   if (tf_input_frame_ != config.input_frame) {
+//     tf_input_frame_ = config.input_frame;
+//     NODELET_DEBUG("[config_callback] Setting the input TF frame to: %s.", tf_input_frame_.c_str());
+//   }
+//   if (tf_output_frame_ != config.output_frame) {
+//     tf_output_frame_ = config.output_frame;
+//     NODELET_DEBUG(
+//       "[config_callback] Setting the output TF frame to: %s.", tf_output_frame_.c_str());
+//   }
+//   // ]---
+// }
 
 }  // namespace pointcloud_preprocessor
 
-PLUGINLIB_EXPORT_CLASS(pointcloud_preprocessor::PointcloudAccumulatorNodelet, nodelet::Nodelet);
+#include "rclcpp_components/register_node_macro.hpp"
+RCLCPP_COMPONENTS_REGISTER_NODE(pointcloud_preprocessor::PointcloudAccumulatorComponent)
