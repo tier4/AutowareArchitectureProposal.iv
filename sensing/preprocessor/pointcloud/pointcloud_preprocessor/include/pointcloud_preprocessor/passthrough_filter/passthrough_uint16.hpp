41c2a2f7680bdf1ad9a73836008b5379805d84c0
Jilada Eccleston <jilada.eccleston@tier4.jp>
2020-12-14 16:31:09 +0900

    Address PR Comments:  - Remove the impl file and merge the two header files

--- sensing/preprocessor/pointcloud/pointcloud_preprocessor/include/pointcloud_preprocessor/passthrough_filter/passthrough_uint16.hpp
+++ sensing/preprocessor/pointcloud/pointcloud_preprocessor/include/pointcloud_preprocessor/passthrough_filter/passthrough_uint16.hpp
@@ -40,6 +40,7 @@
 #ifndef PCL_FILTERS_PASSTHROUGH_UINT16_H_
 #define PCL_FILTERS_PASSTHROUGH_UINT16_H_
 
+#include "pcl/common/io.h"
 #include "pcl/filters/filter_indices.h"
 
 namespace pcl
@@ -323,6 +324,120 @@ private:
   bool filter_limit_negative_;
 };
 }  // namespace pcl
+
+template<typename PointT>
+void pcl::PassThroughUInt16<PointT>::applyFilter(PointCloud & output)
+{
+  std::vector<int> indices;
+  if (keep_organized_) {
+    bool temp = extract_removed_indices_;
+    extract_removed_indices_ = true;
+    applyFilterIndices(indices);
+    extract_removed_indices_ = temp;
+
+    output = *input_;
+    for (int rii = 0; rii < static_cast<int>(removed_indices_->size());
+      ++rii)     // rii = removed indices iterator
+    {
+      output.points[(*removed_indices_)[rii]].x = output.points[(*removed_indices_)[rii]].y =
+        output.points[(*removed_indices_)[rii]].z = user_filter_value_;
+    }
+    if (!std::isfinite(user_filter_value_)) {output.is_dense = false;}
+  } else {
+    output.is_dense = true;
+    applyFilterIndices(indices);
+    copyPointCloud(*input_, indices, output);
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template<typename PointT>
+void pcl::PassThroughUInt16<PointT>::applyFilterIndices(std::vector<int> & indices)
+{
+  // The arrays to be used
+  indices.resize(indices_->size());
+  removed_indices_->resize(indices_->size());
+  int oii = 0, rii = 0;  // oii = output indices iterator, rii = removed indices iterator
+
+  // Has a field name been specified?
+  if (filter_field_name_.empty()) {
+    // Only filter for non-finite entries then
+    for (int iii = 0; iii < static_cast<int>(indices_->size());
+      ++iii)     // iii = input indices iterator
+    {
+      // Non-finite entries are always passed to removed indices
+      if (
+        !std::isfinite(input_->points[(*indices_)[iii]].x) ||
+        !std::isfinite(input_->points[(*indices_)[iii]].y) ||
+        !std::isfinite(input_->points[(*indices_)[iii]].z))
+      {
+        if (extract_removed_indices_) {(*removed_indices_)[rii++] = (*indices_)[iii];}
+        continue;
+      }
+      indices[oii++] = (*indices_)[iii];
+    }
+  } else {
+    // Attempt to get the field name's index
+    std::vector<pcl::PCLPointField> fields;
+    int distance_idx = pcl::getFieldIndex<PointT>(filter_field_name_, fields);
+    if (distance_idx == -1) {
+      PCL_WARN(
+        "[pcl::%s::applyFilter] Unable to find field name in point type.\n",
+        getClassName().c_str());
+      indices.clear();
+      removed_indices_->clear();
+      return;
+    }
+
+    // Filter for non-finite entries and the specified field limits
+    for (int iii = 0; iii < static_cast<int>(indices_->size());
+      ++iii)     // iii = input indices iterator
+    {
+      // Non-finite entries are always passed to removed indices
+      if (
+        !std::isfinite(input_->points[(*indices_)[iii]].x) ||
+        !std::isfinite(input_->points[(*indices_)[iii]].y) ||
+        !std::isfinite(input_->points[(*indices_)[iii]].z))
+      {
+        if (extract_removed_indices_) {(*removed_indices_)[rii++] = (*indices_)[iii];}
+        continue;
+      }
+
+      // Get the field's value
+      const std::uint8_t * pt_data =
+        reinterpret_cast<const std::uint8_t *>(&input_->points[(*indices_)[iii]]);
+      std::uint16_t field_value = 0;
+      memcpy(&field_value, pt_data + fields[distance_idx].offset, sizeof(std::uint16_t));
+
+      // Remove NAN/INF/-INF values. We expect passthrough to output clean valid data.
+      if (!std::isfinite(field_value)) {
+        if (extract_removed_indices_) {(*removed_indices_)[rii++] = (*indices_)[iii];}
+        continue;
+      }
+
+      // Outside of the field limits are passed to removed indices
+      if (!negative_ && (field_value < filter_limit_min_ || field_value > filter_limit_max_)) {
+        if (extract_removed_indices_) {(*removed_indices_)[rii++] = (*indices_)[iii];}
+        continue;
+      }
+
+      // Inside of the field limits are passed to removed indices if negative was set
+      if (negative_ && field_value >= filter_limit_min_ && field_value <= filter_limit_max_) {
+        if (extract_removed_indices_) {(*removed_indices_)[rii++] = (*indices_)[iii];}
+        continue;
+      }
+
+      // Otherwise it was a normal point for output (inlier)
+      indices[oii++] = (*indices_)[iii];
+    }
+  }
+
+  // Resize the output arrays
+  indices.resize(oii);
+  removed_indices_->resize(rii);
+}
+
+#define PCL_INSTANTIATE_PassThroughUInt16(T) template class PCL_EXPORTS pcl::PassThroughUInt16<T>;
 
 #ifdef PCL_NO_PRECOMPILE
 #include "pcl/filters/impl/passthrough.hpp"
