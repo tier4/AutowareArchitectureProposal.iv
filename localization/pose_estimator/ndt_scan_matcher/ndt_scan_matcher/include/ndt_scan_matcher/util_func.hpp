51cc89812a504a5960564eb21611b02f08a052b4
Nithilan <nithilan.karunakaran@tier4.jp>
2020-12-17 14:34:43 +0900

    fixed clang-tidy errors in ndt_scan_matcher

--- localization/pose_estimator/ndt_scan_matcher/ndt_scan_matcher/include/ndt_scan_matcher/util_func.hpp
+++ localization/pose_estimator/ndt_scan_matcher/ndt_scan_matcher/include/ndt_scan_matcher/util_func.hpp
@@ -12,14 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#pragma once
-
+#include <deque>
 #include <algorithm>
 #include <cmath>
 #include <random>
 
 #include "tf2_eigen/tf2_eigen.h"
-
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include "geometry_msgs/msg/pose_array.hpp"
 #include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
 #include "geometry_msgs/msg/twist_stamped.hpp"
