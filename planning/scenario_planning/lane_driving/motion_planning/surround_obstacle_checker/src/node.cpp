be49a2a6c864aa1be693abad39afee9915f67df9
mitsudome-r <ryohsuke.mitsudome@tier4.jp>
2020-12-21 21:12:13 +0900

    [surround_obstacle_checker] fix waitforTransform with lookupTransform for getting transforms
    Signed-off-by: mitsudome-r <ryohsuke.mitsudome@tier4.jp>

--- planning/scenario_planning/lane_driving/motion_planning/surround_obstacle_checker/src/node.cpp
+++ planning/scenario_planning/lane_driving/motion_planning/surround_obstacle_checker/src/node.cpp
@@ -201,15 +201,7 @@ bool SurroundObstacleCheckerNode::convertPose(
 
   try {
     // get transform from source to target
-    auto tf_future =
-      tf_buffer_.waitForTransform(source, target, time, tf2::durationFromSec(0.0), [](auto &) {});
-    auto status = tf_future.wait_for(tf2::durationFromSec(0.1));
-    if (status != std::future_status::ready) {
-      RCLCPP_WARN_STREAM_THROTTLE(
-        get_logger(), *this->get_clock(), 0.5, "cannot get tf from " << source << " to " << target);
-      return false;
-    }
-
+    ros_src2tgt = tf_buffer_.lookupTransform(source, target, time, tf2::durationFromSec(0.1));
     tf2::fromMsg(ros_src2tgt.transform, src2tgt);
   } catch (tf2::TransformException & ex) {
     RCLCPP_WARN_STREAM_THROTTLE(
@@ -258,16 +250,9 @@ void SurroundObstacleCheckerNode::getNearestObstacleByPointCloud(
   // waint to transform pointcloud
   geometry_msgs::msg::TransformStamped transform_stamped;
   try {
-    auto tf_future = tf_buffer_.waitForTransform(
+    transform_stamped = tf_buffer_.lookupTransform(
       "base_link", pointcloud_ptr_->header.frame_id, pointcloud_ptr_->header.stamp,
-      tf2::durationFromSec(0.0), [](auto &) {});
-    auto status = tf_future.wait_for(tf2::durationFromSec(0.5));
-    if (status != std::future_status::ready) {
-      RCLCPP_WARN_STREAM_THROTTLE(
-        get_logger(), *this->get_clock(), 0.5,
-        "failed to get base_link to " << pointcloud_ptr_->header.frame_id << " transform.");
-      return;
-    }
+      tf2::durationFromSec(0.5));
   } catch (tf2::TransformException & ex) {
     RCLCPP_WARN_STREAM_THROTTLE(
       get_logger(), *this->get_clock(), 0.5,
