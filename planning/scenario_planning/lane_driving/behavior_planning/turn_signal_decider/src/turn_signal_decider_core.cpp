1e7ee2167f0ed92c1bb25c7dc74d229eb0453a77
Kazuki Miyahara <kmiya@outlook.com>
2020-12-22 12:45:29 +0900

    remove unused assignment (#1055)

--- planning/scenario_planning/lane_driving/behavior_planning/turn_signal_decider/src/turn_signal_decider_core.cpp
+++ planning/scenario_planning/lane_driving/behavior_planning/turn_signal_decider/src/turn_signal_decider_core.cpp
@@ -112,7 +112,6 @@ void TurnSignalDecider::onTurnSignalTimer()
   }
   if (isTurning(path, vehicle_pose_frenet, &intersection_signal, &distance_to_intersection)) {
     if (min_distance > distance_to_intersection) {
-      min_distance = distance_to_lane_change;
       turn_signal = intersection_signal;
     }
   }
