b3f0b8852e332f69d3bcae2634c69eb2c63bf530
Joshua Whitley <josh.whitley@autoware.org>
2021-01-12 23:34:50 -0600

    Adding more cppcheck ignores for trailing commas in variadic macros

--- src/common/autoware_auto_geometry/test/include/test_spatial_hash.hpp
+++ src/common/autoware_auto_geometry/test/include/test_spatial_hash.hpp
@@ -78,6 +78,7 @@ protected:
 
 // Instantiate tests for given types, add more types here as they are used
 using PointTypesSpatialHash = ::testing::Types<geometry_msgs::msg::Point32>;
+// cppcheck-suppress syntaxError
 TYPED_TEST_CASE(TypedSpatialHashTest, PointTypesSpatialHash, );
 /// NOTE: This is the older version due to 1.8.0 of GTest. v1.8.1 uses TYPED_TEST_SUITE
 
