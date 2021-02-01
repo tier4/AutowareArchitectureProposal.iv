TakaHoribe/autoware.iv.universe

Feature/compensate lidar delay to acc (#1032)
* add compensation to calc dist

* change compensation way

* delete debug output

planning/scenario_planning/lane_driving/motion_planning/obstacle_stop_planner/include/obstacle_stop_planner/adaptive_cruise_control.hpp
@@ -135,6 +135,9 @@ private:
     //!< @brief margin to insert upper velocity
     double margin_rate_to_change_vel;
 
+    //!< @brief use time-compensation to calculate distance to forward vehicle
+    bool use_time_compensation_to_dist;
+
     //!< @brief gain of lowpass filter of upper velocity
     double lowpass_gain_;
 
@@ -168,7 +171,7 @@ private:
   void calcDistanceToNearestPointOnPath(
     const autoware_planning_msgs::Trajectory & trajectory, const int nearest_point_idx,
     const geometry_msgs::Pose & self_pose, const pcl::PointXYZ & nearest_collision_point,
-    double * distance);
+    const ros::Time & nearest_collision_point_time, double * distance);
   double calcTrajYaw(
     const autoware_planning_msgs::Trajectory & trajectory, const int collision_point_idx);
   bool estimatePointVelocityFromObject(

tier4/AutowareArchitectureProposal.iv

planning/scenario_planning/lane_driving/motion_planning/obstacle_stop_planner/include/obstacle_stop_planner/adaptive_cruise_control.hpp
@@ -136,6 +136,9 @@ private:
     //!< @brief margin to insert upper velocity
     double margin_rate_to_change_vel;
 
+    //!< @brief use time-compensation to calculate distance to forward vehicle
+    bool use_time_compensation_to_dist;
+
     //!< @brief gain of lowpass filter of upper velocity
     double lowpass_gain_;
 
@@ -169,7 +172,7 @@ private:
   void calcDistanceToNearestPointOnPath(
     const autoware_planning_msgs::msg::Trajectory & trajectory, const int nearest_point_idx,
     const geometry_msgs::msg::Pose & self_pose, const pcl::PointXYZ & nearest_collision_point,
-    double * distance);
+    const rclcpp::Time & nearest_collision_point_time, double * distance);
   double calcTrajYaw(
     const autoware_planning_msgs::msg::Trajectory & trajectory, const int collision_point_idx);
   bool estimatePointVelocityFromObject(