TakaHoribe/autoware.iv.universe

motion_velocity_optimizer: add closest jerk publisher (#934)
Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>

planning/scenario_planning/common/motion_velocity_optimizer/include/motion_velocity_optimizer/motion_velocity_optimizer.hpp
@@ -204,4 +204,11 @@ private:
   ros::Publisher debug_closest_velocity_;
   ros::Publisher debug_closest_acc_;
   void publishFloat(const double & data, const ros::Publisher & pub) const;
+
+  // jerk calc
+  ros::Publisher debug_closest_jerk_;
+  std::shared_ptr<ros::Time> prev_time_;
+  double prev_acc_;
+  void publishClosestJerk(const double curr_acc);
+
 };

tier4/AutowareArchitectureProposal.iv

planning/scenario_planning/common/motion_velocity_optimizer/include/motion_velocity_optimizer/motion_velocity_optimizer.hpp
@@ -32,6 +32,7 @@
 
 #include "geometry_msgs/msg/twist_stamped.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp/time.hpp"
 #include "tf2/utils.h"
 #include "tf2_ros/transform_listener.h"
 
@@ -198,6 +199,12 @@ private:
     const double & data,
     const rclcpp::Publisher<autoware_debug_msgs::msg::Float32Stamped>::SharedPtr pub)
   const;
+
+  // jerk calc
+  rclcpp::Publisher<autoware_debug_msgs::msg::Float32Stamped>::SharedPtr debug_closest_jerk_;
+  std::shared_ptr<rclcpp::Time> prev_time_;
+  double prev_acc_;
+  void publishClosestJerk(const double curr_acc);
 };
 
 #endif  // MOTION_VELOCITY_OPTIMIZER__MOTION_VELOCITY_OPTIMIZER_HPP_