TakaHoribe/autoware.iv.universe

Feature/improve fail safe architecture (#1120)
* Update msg

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Fix autoware_error_monitor

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Fix emergency_handler

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Fix autoware api

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Add comment

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

system/autoware_error_monitor/src/autoware_error_monitor_node/autoware_error_monitor_node.cpp
@@ -16,22 +16,94 @@
 
 #include <autoware_error_monitor/autoware_error_monitor_node.h>
 
+#include <regex>
+
 #include <fmt/format.h>
+
+#include <autoware_error_monitor/diagnostics_filter.h>
+
+namespace
+{
+int str2level(const std::string & level_str)
+{
+  using diagnostic_msgs::DiagnosticStatus;
+  using std::regex_constants::icase;
+
+  if (std::regex_match(level_str, std::regex("warn", icase))) return DiagnosticStatus::WARN;
+  if (std::regex_match(level_str, std::regex("error", icase))) return DiagnosticStatus::ERROR;
+  if (std::regex_match(level_str, std::regex("stale", icase))) return DiagnosticStatus::STALE;
+
+  throw std::runtime_error(fmt::format("invalid level: {}", level_str));
+};
+
+bool isOverLevel(const int & diag_level, const std::string & failure_level_str)
+{
+  if (failure_level_str == "none") {
+    return false;
+  }
+
+  return diag_level >= str2level(failure_level_str);
+};
+
+std::vector<diagnostic_msgs::DiagnosticStatus> & getTargetDiagnosticsRef(
+  const int hazard_level, autoware_system_msgs::HazardStatus * hazard_status)
+{
+  using autoware_system_msgs::HazardStatus;
+
+  if (hazard_level == HazardStatus::NO_FAULT) return hazard_status->diagnostics_nf;
+  if (hazard_level == HazardStatus::SAFE_FAULT) return hazard_status->diagnostics_sf;
+  if (hazard_level == HazardStatus::LATENT_FAULT) return hazard_status->diagnostics_lf;
+  if (hazard_level == HazardStatus::SINGLE_POINT_FAULT) return hazard_status->diagnostics_spf;
+
+  throw std::runtime_error(fmt::format("invalid hazard level: {}", hazard_level));
+}
+}  // namespace
+
+AutowareErrorMonitorNode::AutowareErrorMonitorNode()
+{
+  // Parameter
+  private_nh_.param("update_rate", update_rate_, 10.0);
+  private_nh_.param("ignore_missing_diagnostics", ignore_missing_diagnostics_, false);
+  private_nh_.param("add_leaf_diagnostics", add_leaf_diagnostics_, true);
+  loadRequiredModules(KeyName::autonomous_driving);
+  loadRequiredModules(KeyName::remote_control);
+
+  // Subscriber
+  sub_diag_array_ =
+    private_nh_.subscribe("input/diag_array", 1, &AutowareErrorMonitorNode::onDiagArray, this);
+
+  // Publisher
+  pub_driving_capability_ =
+    private_nh_.advertise<autoware_system_msgs::DrivingCapability>("output/driving_capability", 1);
+
+  // Timer
+  timer_ =
+    private_nh_.createTimer(ros::Rate(update_rate_), &AutowareErrorMonitorNode::onTimer, this);
+}
 
 void AutowareErrorMonitorNode::loadRequiredModules(const std::string & key)
 {
   const auto param_key = std::string("required_modules/") + key;
 
-  RequiredModules value;
-  if (!private_nh_.getParam(param_key, value)) {
+  XmlRpc::XmlRpcValue xml;
+  if (!private_nh_.getParam(param_key, xml)) {
     throw std::runtime_error(fmt::format("no parameter found: {}", param_key));
   }
 
-  required_modules_map_.insert(std::make_pair(key, value));
+  RequiredModules required_modules;
+  required_modules.reserve(xml.size());
+
+  for (size_t i = 0; i < xml.size(); ++i) {
+    required_modules.emplace_back(xml[i]);
+  }
+
+  required_modules_map_.insert(std::make_pair(key, required_modules));
 }
 
 void AutowareErrorMonitorNode::onDiagArray(const diagnostic_msgs::DiagnosticArray::ConstPtr & msg)
 {
+  diag_array_ = msg;
+
   const auto & header = msg->header;
 
   for (const auto & diag : msg->status) {
@@ -48,40 +120,27 @@ void AutowareErrorMonitorNode::onDiagArray(const diagnostic_msgs::DiagnosticArra
   }
 }
 
-void AutowareErrorMonitorNode::onTimer(const ros::TimerEvent & event)
+bool AutowareErrorMonitorNode::isDataReady()
 {
-  autoware_system_msgs::DrivingCapability driving_capability;
-
-  driving_capability.manual_driving = judgeCapability(KeyName::manual_driving);
-  driving_capability.autonomous_driving = judgeCapability(KeyName::autonomous_driving);
-  driving_capability.remote_control = judgeCapability(KeyName::remote_control);
-  driving_capability.safe_stop = judgeCapability(KeyName::safe_stop);
-  driving_capability.emergency_stop = judgeCapability(KeyName::emergency_stop);
-
-  pub_driving_capability_.publish(driving_capability);
-}
-
-bool AutowareErrorMonitorNode::judgeCapability(const std::string & key)
-{
-  for (const auto & required_module : required_modules_map_.at(key)) {
-    const auto diag_name = required_module;
-    const auto & latest_diag = getLatestDiag(diag_name);
-
-    if (!latest_diag) {
-      return false;
-    }
-
-    if (latest_diag->status.level != diagnostic_msgs::DiagnosticStatus::OK) {
-      return false;
-    }
-
-    const auto time_diff = ros::Time::now() - latest_diag->header.stamp;
-    if (time_diff.toSec() > diag_timeout_sec_) {
-      return false;
-    }
+  if (!diag_array_) {
+    ROS_INFO_THROTTLE(5.0, "waiting for diag_array msg...");
+    return false;
   }
 
   return true;
+}
+void AutowareErrorMonitorNode::onTimer(const ros::TimerEvent & event)
+{
+  if (!isDataReady()) {
+    return;
+  }
+
+  autoware_system_msgs::DrivingCapability driving_capability;
+
+  driving_capability.autonomous_driving = judgeHazardStatus(KeyName::autonomous_driving);
+  driving_capability.remote_control = judgeHazardStatus(KeyName::remote_control);
+
+  pub_driving_capability_.publish(driving_capability);
 }
 
 boost::optional<DiagStamped> AutowareErrorMonitorNode::getLatestDiag(const std::string & diag_name)
@@ -99,25 +158,83 @@ boost::optional<DiagStamped> AutowareErrorMonitorNode::getLatestDiag(const std::
   return diag_buffer.back();
 }
 
-AutowareErrorMonitorNode::AutowareErrorMonitorNode()
+int AutowareErrorMonitorNode::getHazardLevel(
+  const DiagConfig & required_module, const int diag_level)
 {
-  // Parameter
-  private_nh_.param("update_rate", update_rate_, 10.0);
-  loadRequiredModules(KeyName::manual_driving);
-  loadRequiredModules(KeyName::autonomous_driving);
-  loadRequiredModules(KeyName::remote_control);
-  loadRequiredModules(KeyName::safe_stop);
-  loadRequiredModules(KeyName::emergency_stop);
+  using autoware_system_msgs::HazardStatus;
 
-  // Subscriber
-  sub_diag_array_ =
-    private_nh_.subscribe("input/diag_array", 1, &AutowareErrorMonitorNode::onDiagArray, this);
+  if (isOverLevel(diag_level, required_module.spf_at)) return HazardStatus::SINGLE_POINT_FAULT;
+  if (isOverLevel(diag_level, required_module.lf_at)) return HazardStatus::LATENT_FAULT;
+  if (isOverLevel(diag_level, required_module.sf_at)) return HazardStatus::SAFE_FAULT;
 
-  // Publisher
-  pub_driving_capability_ =
-    private_nh_.advertise<autoware_system_msgs::DrivingCapability>("output/driving_capability", 1);
+  return HazardStatus::NO_FAULT;
+}
 
-  // Timer
-  timer_ =
-    private_nh_.createTimer(ros::Rate(update_rate_), &AutowareErrorMonitorNode::onTimer, this);
+void AutowareErrorMonitorNode::appendHazardDiag(
+  const DiagConfig & required_module, const diagnostic_msgs::DiagnosticStatus & hazard_diag,
+  autoware_system_msgs::HazardStatus * hazard_status)
+{
+  const auto hazard_level = getHazardLevel(required_module, hazard_diag.level);
+
+  auto & target_diagnostics_ref = getTargetDiagnosticsRef(hazard_level, hazard_status);
+  target_diagnostics_ref.push_back(hazard_diag);
+
+  if (add_leaf_diagnostics_) {
+    for (const auto & diag :
+         diagnostics_filter::extractLeafChildrenDiagnostics(hazard_diag, diag_array_->status)) {
+      target_diagnostics_ref.push_back(diag);
+    }
+  }
+
+  hazard_status->level = std::max(hazard_status->level, hazard_level);
+}
+
+autoware_system_msgs::HazardStatus AutowareErrorMonitorNode::judgeHazardStatus(
+  const std::string & key)
+{
+  using autoware_system_msgs::HazardStatus;
+  using diagnostic_msgs::DiagnosticStatus;
+
+  autoware_system_msgs::HazardStatus hazard_status;
+
+  for (const auto & required_module : required_modules_map_.at(key)) {
+    const auto & diag_name = required_module.name;
+
+    const auto latest_diag = getLatestDiag(diag_name);
+
+    // no diag found
+    if (!latest_diag) {
+      if (!ignore_missing_diagnostics_) {
+        DiagnosticStatus missing_diag;
+
+        missing_diag.name = diag_name;
+        missing_diag.hardware_id = "autoware_error_monitor";
+        missing_diag.level = DiagnosticStatus::STALE;
+        missing_diag.message = "no diag found";
+
+        appendHazardDiag(required_module, missing_diag, &hazard_status);
+      }
+
+      continue;
+    }
+
+    // diag level high
+    {
+      appendHazardDiag(required_module, latest_diag->status, &hazard_status);
+    }
+
+    // diag timeout
+    {
+      const auto time_diff = ros::Time::now() - latest_diag->header.stamp;
+      if (time_diff.toSec() > diag_timeout_sec_) {
+        DiagnosticStatus timeout_diag = latest_diag->status;
+        timeout_diag.level = DiagnosticStatus::STALE;
+        timeout_diag.message = "timeout";
+
+        appendHazardDiag(required_module, timeout_diag, &hazard_status);
+      }
+    }
+  }
+
+  return hazard_status;
 }

tier4/AutowareArchitectureProposal.iv

system/autoware_error_monitor/src/autoware_error_monitor_node/autoware_error_monitor_node.cpp
@@ -11,21 +11,62 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+#include <regex>
+
 #define FMT_HEADER_ONLY
 #include "fmt/format.h"
 
 #include "autoware_error_monitor/autoware_error_monitor_core.hpp"
 
+#include "autoware_error_monitor/diagnostics_filter.hpp"
+
+namespace
+{
+int str2level(const std::string & level_str)
+{
+  using diagnostic_msgs::msg::DiagnosticStatus;
+  using std::regex_constants::icase;
+
+  if (std::regex_match(level_str, std::regex("warn", icase))) return DiagnosticStatus::WARN;
+  if (std::regex_match(level_str, std::regex("error", icase))) return DiagnosticStatus::ERROR;
+  if (std::regex_match(level_str, std::regex("stale", icase))) return DiagnosticStatus::STALE;
+
+  throw std::runtime_error(fmt::format("invalid level: {}", level_str));
+}
+
+bool isOverLevel(const int & diag_level, const std::string & failure_level_str)
+{
+  if (failure_level_str == "none") {
+    return false;
+  }
+
+  return diag_level >= str2level(failure_level_str);
+}
+
+std::vector<diagnostic_msgs::msg::DiagnosticStatus> & getTargetDiagnosticsRef(
+  const int hazard_level, autoware_system_msgs::msg::HazardStatus * hazard_status)
+{
+  using autoware_system_msgs::msg::HazardStatus;
+
+  if (hazard_level == HazardStatus::NO_FAULT) return hazard_status->diagnostics_nf;
+  if (hazard_level == HazardStatus::SAFE_FAULT) return hazard_status->diagnostics_sf;
+  if (hazard_level == HazardStatus::LATENT_FAULT) return hazard_status->diagnostics_lf;
+  if (hazard_level == HazardStatus::SINGLE_POINT_FAULT) return hazard_status->diagnostics_spf;
+
+  throw std::runtime_error(fmt::format("invalid hazard level: {}", hazard_level));
+}
+}  // namespace
+
 AutowareErrorMonitor::AutowareErrorMonitor()
 : Node("autoware_error_monitor"),
-  update_rate_(declare_parameter("update_rate", 10))
+  update_rate_(declare_parameter("update_rate", 10)),
+  ignore_missing_diagnostics_(declare_parameter("ignore_missing_diagnostics", false)),
+  add_leaf_diagnostics_(declare_parameter("add_leaf_diagnostics", true))
 {
   // Parameter
-  loadRequiredModules(KeyName::manual_driving);
   loadRequiredModules(KeyName::autonomous_driving);
   loadRequiredModules(KeyName::remote_control);
-  loadRequiredModules(KeyName::safe_stop);
-  loadRequiredModules(KeyName::emergency_stop);
 
   // Subscriber
   sub_diag_array_ = create_subscription<diagnostic_msgs::msg::DiagnosticArray>(
@@ -49,21 +90,37 @@ AutowareErrorMonitor::AutowareErrorMonitor()
 
 void AutowareErrorMonitor::loadRequiredModules(const std::string & key)
 {
-  const auto param_key = std::string("required_modules.") + key;
+  const auto param_key = std::string("required_modules.") + key + std::string(".names");
 
   this->declare_parameter(param_key);
 
-  RequiredModules value = this->get_parameter(param_key).as_string_array();
-  if (value.size() == 0) {
+  const auto names = this->get_parameter(param_key).as_string_array();
+  if (names.size() == 0) {
     throw std::runtime_error(fmt::format("no parameter found: {}", param_key));
   }
 
-  required_modules_map_.insert(std::make_pair(key, value));
+  RequiredModules required_modules;
+  required_modules.reserve(names.size());
+
+  DiagLevel default_level{{"sf_at", ""}, {"lf_at", ""}, {"spf_at", ""}};
+
+  for (const auto & module_name : names) {
+    const auto diag_level_key =
+      std::string("required_modules.") + key + std::string(".diag_level.") + module_name;
+    this->declare_parameters(diag_level_key, default_level);
+    DiagLevel diag_level{};
+    this->get_parameters(diag_level_key, diag_level);
+    required_modules.emplace_back(module_name, diag_level);
+  }
+
+  required_modules_map_.insert(std::make_pair(key, required_modules));
 }
 
 void AutowareErrorMonitor::onDiagArray(
   const diagnostic_msgs::msg::DiagnosticArray::ConstSharedPtr msg)
 {
+  diag_array_ = msg;
+
   const auto & header = msg->header;
 
   for (const auto & diag : msg->status) {
@@ -80,40 +137,29 @@ void AutowareErrorMonitor::onDiagArray(
   }
 }
 
-void AutowareErrorMonitor::onTimer()
+bool AutowareErrorMonitor::isDataReady()
 {
-  autoware_system_msgs::msg::DrivingCapability driving_capability;
-
-  driving_capability.manual_driving = judgeCapability(KeyName::manual_driving);
-  driving_capability.autonomous_driving = judgeCapability(KeyName::autonomous_driving);
-  driving_capability.remote_control = judgeCapability(KeyName::remote_control);
-  driving_capability.safe_stop = judgeCapability(KeyName::safe_stop);
-  driving_capability.emergency_stop = judgeCapability(KeyName::emergency_stop);
-
-  pub_driving_capability_->publish(driving_capability);
-}
-
-bool AutowareErrorMonitor::judgeCapability(const std::string & key)
-{
-  for (const std::string & required_module : required_modules_map_.at(key)) {
-    const auto diag_name = required_module;
-    const auto & latest_diag = getLatestDiag(diag_name);
-
-    if (!latest_diag) {
-      return false;
-    }
-
-    if (latest_diag->status.level != diagnostic_msgs::msg::DiagnosticStatus::OK) {
-      return false;
-    }
-
-    const auto time_diff = rclcpp::Node::now() - latest_diag->header.stamp;
-    if (time_diff.seconds() > diag_timeout_sec_) {
-      return false;
-    }
+  if (!diag_array_) {
+    RCLCPP_WARN_THROTTLE(
+      get_logger(), *get_clock(), std::chrono::milliseconds(5000).count(), "waiting for diag_array msg...");
+    return false;
   }
 
   return true;
+}
+
+void AutowareErrorMonitor::onTimer()
+{
+  if (!isDataReady()) {
+    return;
+  }
+
+  autoware_system_msgs::msg::DrivingCapability driving_capability;
+
+  driving_capability.autonomous_driving = judgeHazardStatus(KeyName::autonomous_driving);
+  driving_capability.remote_control = judgeHazardStatus(KeyName::remote_control);
+
+  pub_driving_capability_->publish(driving_capability);
 }
 
 boost::optional<DiagStamped> AutowareErrorMonitor::getLatestDiag(const std::string & diag_name)
@@ -129,4 +175,85 @@ boost::optional<DiagStamped> AutowareErrorMonitor::getLatestDiag(const std::stri
   }
 
   return diag_buffer.back();
+}
+
+int AutowareErrorMonitor::getHazardLevel(
+  const DiagConfig & required_module, const int diag_level)
+{
+  using autoware_system_msgs::msg::HazardStatus;
+
+  if (isOverLevel(diag_level, required_module.spf_at)) return HazardStatus::SINGLE_POINT_FAULT;
+  if (isOverLevel(diag_level, required_module.lf_at)) return HazardStatus::LATENT_FAULT;
+  if (isOverLevel(diag_level, required_module.sf_at)) return HazardStatus::SAFE_FAULT;
+
+  return HazardStatus::NO_FAULT;
+}
+
+void AutowareErrorMonitor::appendHazardDiag(
+  const DiagConfig & required_module, const diagnostic_msgs::msg::DiagnosticStatus & hazard_diag,
+  autoware_system_msgs::msg::HazardStatus * hazard_status)
+{
+  const auto hazard_level = getHazardLevel(required_module, hazard_diag.level);
+
+  auto & target_diagnostics_ref = getTargetDiagnosticsRef(hazard_level, hazard_status);
+  target_diagnostics_ref.push_back(hazard_diag);
+
+  if (add_leaf_diagnostics_) {
+    for (const auto & diag :
+         diagnostics_filter::extractLeafChildrenDiagnostics(hazard_diag, diag_array_->status)) {
+      target_diagnostics_ref.push_back(diag);
+    }
+  }
+
+  hazard_status->level = std::max(hazard_status->level, hazard_level);
+}
+
+autoware_system_msgs::msg::HazardStatus AutowareErrorMonitor::judgeHazardStatus(
+  const std::string & key)
+{
+  using autoware_system_msgs::msg::HazardStatus;
+  using diagnostic_msgs::msg::DiagnosticStatus;
+
+  autoware_system_msgs::msg::HazardStatus hazard_status;
+
+  for (const auto & required_module : required_modules_map_.at(key)) {
+    const auto & diag_name = required_module.name;
+
+    const auto latest_diag = getLatestDiag(diag_name);
+
+    // no diag found
+    if (!latest_diag) {
+      if (!ignore_missing_diagnostics_) {
+        DiagnosticStatus missing_diag;
+
+        missing_diag.name = diag_name;
+        missing_diag.hardware_id = "autoware_error_monitor";
+        missing_diag.level = DiagnosticStatus::STALE;
+        missing_diag.message = "no diag found";
+
+        appendHazardDiag(required_module, missing_diag, &hazard_status);
+      }
+
+      continue;
+    }
+
+    // diag level high
+    {
+      appendHazardDiag(required_module, latest_diag->status, &hazard_status);
+    }
+
+    // diag timeout
+    {
+      const auto time_diff = this->now() - latest_diag->header.stamp;
+      if (time_diff.seconds() > diag_timeout_sec_) {
+        DiagnosticStatus timeout_diag = latest_diag->status;
+        timeout_diag.level = DiagnosticStatus::STALE;
+        timeout_diag.message = "timeout";
+
+        appendHazardDiag(required_module, timeout_diag, &hazard_status);
+      }
+    }
+  }
+
+  return hazard_status;
 }