TakaHoribe/autoware.iv.universe

Add emergency monitoring in vehicle_cmd_gate (#809)
* Add emergency monitoring in vehicle_cmd_gate

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Rename variables

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Fix shift

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Use function

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Move comment

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Fix comment

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Fix bug

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Apply review

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Fix log level

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Change parameter order

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

control/vehicle_cmd_gate/include/vehicle_cmd_gate/vehicle_cmd_gate.h
@@ -66,6 +66,10 @@ private:
   bool is_emergency_;
   autoware_control_msgs::GateMode current_gate_mode_;
 
+  // Heartbeat
+  ros::Time emergency_heartbeat_received_time_;
+  bool is_emergency_heartbeat_timeout_ = false;
+
   // Subscriber for auto
   Commands auto_commands_;
   ros::Subscriber auto_control_cmd_sub_;
@@ -96,16 +100,19 @@ private:
   // Parameter
   double update_rate_;
   bool use_emergency_handling_;
+  double emergency_heartbeat_timeout_;
 
   // Timer / Event
   ros::Timer timer_;
 
   void onTimer(const ros::TimerEvent & event);
   void publishControlCommands(const Commands & input_msg);
+  void publishEmergencyControlCommands();
 
   // Algorithm
   autoware_control_msgs::ControlCommand prev_control_cmd_;
   autoware_control_msgs::ControlCommand createStopControlCmd() const;
+  autoware_control_msgs::ControlCommand createEmergencyStopControlCmd() const;
 
   std::shared_ptr<ros::Time> prev_time_;
   double getDt();

tier4/AutowareArchitectureProposal.iv

control/vehicle_cmd_gate/include/vehicle_cmd_gate/vehicle_cmd_gate.h
@@ -62,7 +62,11 @@ private:
   bool is_emergency_;
   autoware_control_msgs::msg::GateMode current_gate_mode_;
 
-  // Subscription for auto
+  // Heartbeat
+  rclcpp::Time emergency_heartbeat_received_time_;
+  bool is_emergency_heartbeat_timeout_ = false;
+
+  // Subscriber for auto
   Commands auto_commands_;
   rclcpp::Subscription<autoware_control_msgs::msg::ControlCommandStamped>::SharedPtr
     auto_control_cmd_sub_;
@@ -98,16 +102,19 @@ private:
   // Parameter
   double update_period_;
   bool use_emergency_handling_;
+  double emergency_heartbeat_timeout_;
 
   // Timer / Event
   rclcpp::TimerBase::SharedPtr timer_;
 
   void onTimer();
   void publishControlCommands(const Commands & input_msg);
+  void publishEmergencyControlCommands();
 
   // Algorithm
   autoware_control_msgs::msg::ControlCommand prev_control_cmd_;
   autoware_control_msgs::msg::ControlCommand createStopControlCmd() const;
+  autoware_control_msgs::msg::ControlCommand createEmergencyStopControlCmd() const;
 
   std::shared_ptr<rclcpp::Time> prev_time_;
   double getDt();