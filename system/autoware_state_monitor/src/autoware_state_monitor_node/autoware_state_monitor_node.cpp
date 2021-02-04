TakaHoribe/autoware.iv.universe

Check if engaged before setting disengage (#1131)
Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

system/autoware_state_monitor/src/autoware_state_monitor_node/autoware_state_monitor_node.cpp
@@ -137,7 +137,7 @@ void AutowareStateMonitorNode::onRoute(const autoware_planning_msgs::Route::Cons
     state_input_.goal_pose = geometry_msgs::Pose::ConstPtr(p);
   }
 
-  if (disengage_on_route_) {
+  if (disengage_on_route_ && isEngaged()) {
     ROS_INFO("new route received and disengage Autoware");
     setDisengage();
   }
@@ -211,7 +211,7 @@ void AutowareStateMonitorNode::onTimer(const ros::TimerEvent & event)
   }
 
   // Disengage on event
-  if (disengage_on_goal_ && autoware_state == AutowareState::ArrivedGoal) {
+  if (disengage_on_goal_ && isEngaged() && autoware_state == AutowareState::ArrivedGoal) {
     ROS_INFO("arrived goal and disengage Autoware");
     setDisengage();
   }
@@ -351,6 +351,15 @@ TfStats AutowareStateMonitorNode::getTfStats() const
   }
 
   return tf_stats;
+}
+
+bool AutowareStateMonitorNode::isEngaged()
+{
+  if (!state_input_.autoware_engage) {
+    return false;
+  }
+
+  return state_input_.autoware_engage->data;
 }
 
 void AutowareStateMonitorNode::setDisengage()

tier4/AutowareArchitectureProposal.iv

system/autoware_state_monitor/src/autoware_state_monitor_node/autoware_state_monitor_node.cpp
@@ -136,7 +136,7 @@ void AutowareStateMonitorNode::onRoute(const autoware_planning_msgs::msg::Route:
     state_input_.goal_pose = geometry_msgs::msg::Pose::ConstSharedPtr(p);
   }
 
-  if (disengage_on_route_) {
+  if (disengage_on_route_ && isEngaged()) {
     RCLCPP_INFO(this->get_logger(), "new route received and disengage Autoware");
     setDisengage();
   }
@@ -214,7 +214,7 @@ void AutowareStateMonitorNode::onTimer()
   }
 
   // Disengage on event
-  if (disengage_on_goal_ && autoware_state == AutowareState::ArrivedGoal) {
+  if (disengage_on_goal_ && isEngaged() && autoware_state == AutowareState::ArrivedGoal) {
     RCLCPP_INFO(this->get_logger(), "arrived goal and disengage Autoware");
     setDisengage();
   }
@@ -359,6 +359,15 @@ TfStats AutowareStateMonitorNode::getTfStats() const
   }
 
   return tf_stats;
+}
+
+bool AutowareStateMonitorNode::isEngaged()
+{
+  if (!state_input_.autoware_engage) {
+    return false;
+  }
+
+  return state_input_.autoware_engage->is_engaged;
 }
 
 void AutowareStateMonitorNode::setDisengage()