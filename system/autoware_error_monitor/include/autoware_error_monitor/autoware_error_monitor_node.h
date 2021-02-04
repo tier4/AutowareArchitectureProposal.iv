TakaHoribe/autoware.iv.universe

Refactor autoware error monitor (#848)
* Rename conditions to modules

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Change parameter style

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Fixup

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

system/autoware_error_monitor/include/autoware_error_monitor/autoware_error_monitor_node.h
@@ -33,16 +33,16 @@ struct DiagStamped
   diagnostic_msgs::DiagnosticStatus status;
 };
 
-using RequiredConditions = std::vector<std::string>;
+using RequiredModules = std::vector<std::string>;
 using DiagBuffer = std::deque<DiagStamped>;
 
 struct KeyName
 {
-  static constexpr const char * manual_driving = "/manual_driving";
-  static constexpr const char * autonomous_driving = "/autonomous_driving";
-  static constexpr const char * remote_control = "/remote_control";
-  static constexpr const char * safe_stop = "/safe_stop";
-  static constexpr const char * emergency_stop = "/emergency_stop";
+  static constexpr const char * manual_driving = "manual_driving";
+  static constexpr const char * autonomous_driving = "autonomous_driving";
+  static constexpr const char * remote_control = "remote_control";
+  static constexpr const char * safe_stop = "safe_stop";
+  static constexpr const char * emergency_stop = "emergency_stop";
 };
 
 class AutowareErrorMonitorNode
@@ -57,9 +57,9 @@ private:
 
   // Parameter
   double update_rate_;
-  std::unordered_map<std::string, RequiredConditions> required_conditions_map_;
+  std::unordered_map<std::string, RequiredModules> required_modules_map_;
 
-  void loadRequiredConditions(const std::string & key);
+  void loadRequiredModules(const std::string & key);
 
   // Timer
   ros::Timer timer_;

system/autoware_error_monitor/src/autoware_error_monitor_node/autoware_error_monitor_node.cpp
@@ -18,16 +18,16 @@
 
 #include <fmt/format.h>
 
-void AutowareErrorMonitorNode::loadRequiredConditions(const std::string & key)
+void AutowareErrorMonitorNode::loadRequiredModules(const std::string & key)
 {
-  const auto param_key = std::string("required_conditions") + key;
+  const auto param_key = std::string("required_modules/") + key;
 
-  RequiredConditions value;
+  RequiredModules value;
   if (!private_nh_.getParam(param_key, value)) {
     throw std::runtime_error(fmt::format("no parameter found: {}", param_key));
   }
 
-  required_conditions_map_.insert(std::make_pair(key, value));
+  required_modules_map_.insert(std::make_pair(key, value));
 }
 
 void AutowareErrorMonitorNode::onDiagArray(const diagnostic_msgs::DiagnosticArray::ConstPtr & msg)
@@ -63,8 +63,8 @@ void AutowareErrorMonitorNode::onTimer(const ros::TimerEvent & event)
 
 bool AutowareErrorMonitorNode::judgeCapability(const std::string & key)
 {
-  for (const auto & required_condition : required_conditions_map_.at(key)) {
-    const auto diag_name = fmt::format("/{}", required_condition);
+  for (const auto & required_module : required_modules_map_.at(key)) {
+    const auto diag_name = required_module;
     const auto & latest_diag = getLatestDiag(diag_name);
 
     if (!latest_diag) {
@@ -103,11 +103,11 @@ AutowareErrorMonitorNode::AutowareErrorMonitorNode()
 {
   // Parameter
   private_nh_.param("update_rate", update_rate_, 10.0);
-  loadRequiredConditions(KeyName::manual_driving);
-  loadRequiredConditions(KeyName::autonomous_driving);
-  loadRequiredConditions(KeyName::remote_control);
-  loadRequiredConditions(KeyName::safe_stop);
-  loadRequiredConditions(KeyName::emergency_stop);
+  loadRequiredModules(KeyName::manual_driving);
+  loadRequiredModules(KeyName::autonomous_driving);
+  loadRequiredModules(KeyName::remote_control);
+  loadRequiredModules(KeyName::safe_stop);
+  loadRequiredModules(KeyName::emergency_stop);
 
   // Subscriber
   sub_diag_array_ =

tier4/AutowareArchitectureProposal.iv

system/autoware_error_monitor/include/autoware_error_monitor/autoware_error_monitor_node.h
@@ -30,7 +30,7 @@ struct DiagStamped
   diagnostic_msgs::msg::DiagnosticStatus status;
 };
 
-using RequiredConditions = std::vector<std::string>;
+using RequiredModules = std::vector<std::string>;
 using DiagBuffer = std::deque<DiagStamped>;
 
 struct KeyName
@@ -50,9 +50,9 @@ public:
 private:
   // Parameter
   int update_rate_;
-  std::unordered_map<std::string, RequiredConditions> required_conditions_map_;
+  std::unordered_map<std::string, RequiredConditions> required_modules_map_;
 
-  void loadRequiredConditions(const std::string & key);
+  void loadRequiredModules(const std::string & key);
 
   // Timer
   rclcpp::TimerBase::SharedPtr timer_;

system/autoware_error_monitor/src/autoware_error_monitor_node/autoware_error_monitor_node.cpp
@@ -21,11 +21,11 @@ AutowareErrorMonitor::AutowareErrorMonitor()
   update_rate_(declare_parameter("update_rate", 10))
 {
   // Parameter
-  loadRequiredConditions(KeyName::manual_driving);
-  loadRequiredConditions(KeyName::autonomous_driving);
-  loadRequiredConditions(KeyName::remote_control);
-  loadRequiredConditions(KeyName::safe_stop);
-  loadRequiredConditions(KeyName::emergency_stop);
+  loadRequiredModules(KeyName::manual_driving);
+  loadRequiredModules(KeyName::autonomous_driving);
+  loadRequiredModules(KeyName::remote_control);
+  loadRequiredModules(KeyName::safe_stop);
+  loadRequiredModules(KeyName::emergency_stop);
 
   // Subscriber
   sub_diag_array_ = create_subscription<diagnostic_msgs::msg::DiagnosticArray>(
@@ -47,18 +47,18 @@ AutowareErrorMonitor::AutowareErrorMonitor()
   this->get_node_timers_interface()->add_timer(timer_, nullptr);
 }
 
-void AutowareErrorMonitor::loadRequiredConditions(const std::string & key)
+void AutowareErrorMonitor::loadRequiredModules(const std::string & key)
 {
-  const auto param_key = std::string("required_conditions.") + key;
+  const auto param_key = std::string("required_modules.") + key;
 
   this->declare_parameter(param_key);
 
-  RequiredConditions value = this->get_parameter(param_key).as_string_array();
+  RequiredModules value = this->get_parameter(param_key).as_string_array();
   if (value.size() == 0) {
     throw std::runtime_error(fmt::format("no parameter found: {}", param_key));
   }
 
-  required_conditions_map_.insert(std::make_pair(key, value));
+  required_modules_map_.insert(std::make_pair(key, value));
 }
 
 void AutowareErrorMonitor::onDiagArray(
@@ -95,8 +95,8 @@ void AutowareErrorMonitor::onTimer()
 
 bool AutowareErrorMonitor::judgeCapability(const std::string & key)
 {
-  for (const std::string & required_condition : required_conditions_map_.at(key)) {
-    const auto diag_name = fmt::format("/{}", required_condition);
+  for (const std::string & required_module : required_modules_map_.at(key)) {
+    const auto diag_name = required_module;
     const auto & latest_diag = getLatestDiag(diag_name);
 
     if (!latest_diag) {