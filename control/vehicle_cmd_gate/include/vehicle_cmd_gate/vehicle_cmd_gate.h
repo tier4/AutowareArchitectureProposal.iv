TakaHoribe/autoware.iv.universe

Improve remote emergency stop (#900)
* Apply format

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Rename emergency to system_emergency in vehicle_cmd_gate

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Add emergency stop feature to vehicle_cmd_gate

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Fix frame_id of vehicle_cmd_gate output

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Rename /remote/emergency to /remote/emergency_stop in autoware_joy_controller

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Rename /remote/emergency to /remote/emergency_stop in remote_cmd_converter

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Rename /remote/emergency to /remote/emergency_stop in autoware_api

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Check emergency_stop timeout in remote_cmd_converter

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Ignore timeout = 0.0

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Add config_file to arg

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Rename emergency_stop to external_emergency_stop

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Remove unnecessary lines

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Wait for first heartbeat

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Add clear_emergency_stop service

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Call clear_external_emegency_stop service from autoware_joy_controller

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Rename function

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Revert: Wait for first heartbeat

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Fix console messages

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Move emergency_stop diag to vehicle_cmd_gate

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Add heartbeat to vehicle_cmd_

control/vehicle_cmd_gate/include/vehicle_cmd_gate/vehicle_cmd_gate.h
@@ -19,14 +19,17 @@
 
 #include <memory>
 
+#include <diagnostic_updater/diagnostic_updater.h>
 #include <ros/ros.h>
-#include <std_msgs/Bool.h>
 
-#include "autoware_control_msgs/ControlCommandStamped.h"
-#include "autoware_control_msgs/GateMode.h"
-#include "autoware_vehicle_msgs/ShiftStamped.h"
-#include "autoware_vehicle_msgs/TurnSignal.h"
-#include "autoware_vehicle_msgs/VehicleCommand.h"
+#include <autoware_control_msgs/ControlCommandStamped.h>
+#include <autoware_control_msgs/GateMode.h>
+#include <autoware_vehicle_msgs/ShiftStamped.h>
+#include <autoware_vehicle_msgs/TurnSignal.h>
+#include <autoware_vehicle_msgs/VehicleCommand.h>
+#include <std_msgs/Bool.h>
+#include <std_srvs/Trigger.h>
+
 #include "vehicle_cmd_gate/vehicle_cmd_filter.h"
 
 struct Commands
@@ -43,8 +46,8 @@ public:
 
 private:
   // NodeHandle
-  ros::NodeHandle nh_;
-  ros::NodeHandle pnh_;
+  ros::NodeHandle nh_{""};
+  ros::NodeHandle pnh_{"~"};
 
   // Publisher
   ros::Publisher vehicle_cmd_pub_;
@@ -55,20 +58,26 @@ private:
 
   // Subscriber
   ros::Subscriber engage_sub_;
-  ros::Subscriber emergency_sub_;
+  ros::Subscriber system_emergency_sub_;
+  ros::Subscriber external_emergency_stop_sub_;
   ros::Subscriber gate_mode_sub_;
 
   void onGateMode(const autoware_control_msgs::GateMode::ConstPtr & msg);
   void onEngage(const std_msgs::Bool::ConstPtr msg);
-  void onEmergency(const std_msgs::Bool::ConstPtr msg);
+  void onSystemEmergency(const std_msgs::Bool::ConstPtr msg);
+  void onExternalEmergencyStop(const std_msgs::Bool::ConstPtr msg);
 
-  bool is_engaged_;
-  bool is_emergency_;
+  bool is_engaged_ = false;
+  bool is_system_emergency_ = false;
+  bool is_external_emergency_stop_ = false;
   autoware_control_msgs::GateMode current_gate_mode_;
 
   // Heartbeat
-  ros::Time emergency_heartbeat_received_time_;
-  bool is_emergency_heartbeat_timeout_ = false;
+  std::shared_ptr<ros::Time> system_emergency_heartbeat_received_time_;
+  bool is_system_emergency_heartbeat_timeout_ = false;
+
+  std::shared_ptr<ros::Time> external_emergency_stop_heartbeat_received_time_;
+  bool is_external_emergency_stop_heartbeat_timeout_ = false;
 
   // Subscriber for auto
   Commands auto_commands_;
@@ -100,14 +109,27 @@ private:
   // Parameter
   double update_rate_;
   bool use_emergency_handling_;
-  double emergency_heartbeat_timeout_;
+  bool use_external_emergency_stop_;
+  double system_emergency_heartbeat_timeout_;
+  double external_emergency_stop_heartbeat_timeout_;
+
+  // Service
+  ros::ServiceServer srv_clear_external_emergency_stop_;
+
+  bool onClearExternalEmergencyStopService(
+    std_srvs::Trigger::Request & req, std_srvs::Trigger::Response & res);
 
   // Timer / Event
   ros::Timer timer_;
 
   void onTimer(const ros::TimerEvent & event);
   void publishControlCommands(const Commands & input_msg);
-  void publishEmergencyControlCommands();
+  void publishEmergencyStopControlCommands();
+
+  // Diagnostics Updater
+  diagnostic_updater::Updater updater_;
+
+  void checkExternalEmergencyStop(diagnostic_updater::DiagnosticStatusWrapper & stat);
 
   // Algorithm
   autoware_control_msgs::ControlCommand prev_control_cmd_;

tier4/AutowareArchitectureProposal.iv

control/vehicle_cmd_gate/include/vehicle_cmd_gate/vehicle_cmd_gate.h
@@ -17,6 +17,7 @@
 
 #include <memory>
 
+#include <diagnostic_updater/diagnostic_updater.h>
 #include "rclcpp/rclcpp.hpp"
 
 #include "autoware_control_msgs/msg/control_command_stamped.hpp"
@@ -28,6 +29,7 @@
 #include "autoware_vehicle_msgs/msg/vehicle_command.hpp"
 
 #include "vehicle_cmd_gate/vehicle_cmd_filter.hpp"
+#include <std_srvs/srv/trigger.h>
 
 struct Commands
 {
@@ -51,20 +53,26 @@ private:
 
   // Subscription
   rclcpp::Subscription<autoware_control_msgs::msg::EngageMode>::SharedPtr engage_sub_;
-  rclcpp::Subscription<autoware_control_msgs::msg::EmergencyMode>::SharedPtr emergency_sub_;
+  rclcpp::Subscription<autoware_control_msgs::msg::EmergencyMode>::SharedPtr system_emergency_sub_;
+  rclcpp::Subscription<autoware_control_msgs::msg::EmergencyMode>::SharedPtr external_emergency_stop_sub_;
   rclcpp::Subscription<autoware_control_msgs::msg::GateMode>::SharedPtr gate_mode_sub_;
 
   void onGateMode(autoware_control_msgs::msg::GateMode::ConstSharedPtr msg);
   void onEngage(autoware_control_msgs::msg::EngageMode::ConstSharedPtr msg);
-  void onEmergency(autoware_control_msgs::msg::EmergencyMode::ConstSharedPtr msg);
+  void onSystemEmergency(autoware_control_msgs::msg::EmergencyMode::ConstSharedPtr msg);
+  void onExternalEmergencyStop(autoware_control_msgs::msg::EmergencyMode::ConstSharedPtr msg);
 
   bool is_engaged_;
-  bool is_emergency_;
+  bool is_system_emergency_ = false;
+  bool is_external_emergency_stop_ = false;
   autoware_control_msgs::msg::GateMode current_gate_mode_;
 
   // Heartbeat
-  rclcpp::Time emergency_heartbeat_received_time_;
-  bool is_emergency_heartbeat_timeout_ = false;
+  std::shared_ptr<rclcpp::Time> system_emergency_heartbeat_received_time_;
+  bool is_system_emergency_heartbeat_timeout_ = false;
+
+  std::shared_ptr<rclcpp::Time> external_emergency_stop_heartbeat_received_time_;
+  bool is_external_emergency_stop_heartbeat_timeout_ = false;
 
   // Subscriber for auto
   Commands auto_commands_;
@@ -102,14 +110,27 @@ private:
   // Parameter
   double update_period_;
   bool use_emergency_handling_;
-  double emergency_heartbeat_timeout_;
+  bool use_external_emergency_stop_;
+  double system_emergency_heartbeat_timeout_;
+  double external_emergency_stop_heartbeat_timeout_;
+
+  // Service
+  ros::ServiceServer srv_clear_external_emergency_stop_;
+
+  bool onClearExternalEmergencyStopService(
+    std_srvs::Trigger::Request & req, std_srvs::Trigger::Response & res);
 
   // Timer / Event
   rclcpp::TimerBase::SharedPtr timer_;
 
   void onTimer();
   void publishControlCommands(const Commands & input_msg);
-  void publishEmergencyControlCommands();
+  void publishEmergencyStopControlCommands();
+
+  // Diagnostics Updater
+  diagnostic_updater::Updater updater_;
+
+  void checkExternalEmergencyStop(diagnostic_updater::DiagnosticStatusWrapper & stat);
 
   // Algorithm
   autoware_control_msgs::msg::ControlCommand prev_control_cmd_;