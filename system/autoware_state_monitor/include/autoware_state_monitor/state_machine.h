TakaHoribe/autoware.iv.universe

Fix emergency handler (#1065)
* Don't wait for twist in order to output topic before initialized

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Ignore Initializing state

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Add wait time after initialization in autoware_state_monitor

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Replace ROS_DEBUG with ROS_INFO

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Don't wait for prev_control_command

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

* Allow transition to Emergency from any state

Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

system/autoware_state_monitor/include/autoware_state_monitor/state_machine.h
@@ -60,7 +60,14 @@ struct StateParam
 struct Times
 {
   ros::Time arrived_goal;
+  ros::Time initializing_completed;
   ros::Time planning_completed;
+};
+
+struct Flags
+{
+  bool waiting_after_initializing = false;
+  bool waiting_after_planning = false;
 };
 
 class StateMachine
@@ -77,10 +84,11 @@ private:
   StateInput state_input_;
   const StateParam state_param_;
 
+  mutable AutowareState state_before_emergency_ = AutowareState::InitializingVehicle;
   mutable std::vector<std::string> msgs_;
   mutable Times times_;
+  mutable Flags flags_;
   mutable autoware_planning_msgs::Route::ConstPtr executing_route_ = nullptr;
-  mutable bool waiting_after_planning_ = false;
 
   AutowareState judgeAutowareState() const;
 

system/autoware_state_monitor/src/autoware_state_monitor_node/state_machine.cpp
@@ -208,10 +208,27 @@ AutowareState StateMachine::judgeAutowareState() const
     return AutowareState::Finalizing;
   }
 
+  if (autoware_state_ != AutowareState::Emergency && isEmergency()) {
+    state_before_emergency_ = autoware_state_;
+    return AutowareState::Emergency;
+  }
+
   switch (autoware_state_) {
     case (AutowareState::InitializingVehicle): {
       if (isVehicleInitialized()) {
-        return AutowareState::WaitingForRoute;
+        if (!flags_.waiting_after_initializing) {
+          flags_.waiting_after_initializing = true;
+          times_.initializing_completed = ros::Time::now();
+          break;
+        }
+
+        // Wait after initialize completed to avoid sync error
+        constexpr double wait_time_after_initializing = 1.0;
+        const auto time_from_initializing = ros::Time::now() - times_.initializing_completed;
+        if (time_from_initializing.toSec() > wait_time_after_initializing) {
+          flags_.waiting_after_initializing = false;
+          return AutowareState::WaitingForRoute;
+        }
       }
 
       break;
@@ -233,8 +250,8 @@ AutowareState StateMachine::judgeAutowareState() const
       executing_route_ = state_input_.route;
 
       if (isPlanningCompleted()) {
-        if (!waiting_after_planning_) {
-          waiting_after_planning_ = true;
+        if (!flags_.waiting_after_planning) {
+          flags_.waiting_after_planning = true;
           times_.planning_completed = ros::Time::now();
           break;
         }
@@ -243,7 +260,7 @@ AutowareState StateMachine::judgeAutowareState() const
         constexpr double wait_time_after_planning = 1.0;
         const auto time_from_planning = ros::Time::now() - times_.planning_completed;
         if (time_from_planning.toSec() > wait_time_after_planning) {
-          waiting_after_planning_ = false;
+          flags_.waiting_after_planning = false;
           return AutowareState::WaitingForEngage;
         }
       }
@@ -252,10 +269,6 @@ AutowareState StateMachine::judgeAutowareState() const
     }
 
     case (AutowareState::WaitingForEngage): {
-      if (isEmergency()) {
-        return AutowareState::Emergency;
-      }
-
       if (isRouteReceived()) {
         return AutowareState::Planning;
       }
@@ -273,10 +286,6 @@ AutowareState StateMachine::judgeAutowareState() const
     }
 
     case (AutowareState::Driving): {
-      if (isEmergency()) {
-        return AutowareState::Emergency;
-      }
-
       if (isRouteReceived()) {
         return AutowareState::Planning;
       }
@@ -305,7 +314,7 @@ AutowareState StateMachine::judgeAutowareState() const
 
     case (AutowareState::Emergency): {
       if (!isEmergency()) {
-        return AutowareState::WaitingForEngage;
+        return state_before_emergency_;
       }
 
       break;

tier4/AutowareArchitectureProposal.iv

system/autoware_state_monitor/include/autoware_state_monitor/state_machine.h
@@ -63,7 +63,14 @@ struct StateParam
 struct Times
 {
   rclcpp::Time arrived_goal;
+  rclcpp::Time initializing_completed;
   rclcpp::Time planning_completed;
+};
+
+struct Flags
+{
+  bool waiting_after_initializing = false;
+  bool waiting_after_planning = false;
 };
 
 class StateMachine
@@ -81,10 +88,11 @@ private:
   StateInput state_input_;
   const StateParam state_param_;
 
+  mutable AutowareState state_before_emergency_ = AutowareState::InitializingVehicle;
   mutable std::vector<std::string> msgs_;
   mutable Times times_;
+  mutable Flags flags_;
   mutable autoware_planning_msgs::msg::Route::ConstSharedPtr executing_route_ = nullptr;
-  mutable bool waiting_after_planning_ = false;
 
   AutowareState judgeAutowareState() const;
 

system/autoware_state_monitor/src/autoware_state_monitor_node/state_machine.cpp
@@ -207,10 +207,27 @@ AutowareState StateMachine::judgeAutowareState() const
     return AutowareState::Finalizing;
   }
 
+  if (autoware_state_ != AutowareState::Emergency && isEmergency()) {
+    state_before_emergency_ = autoware_state_;
+    return AutowareState::Emergency;
+  }
+
   switch (autoware_state_) {
     case (AutowareState::InitializingVehicle): {
         if (isVehicleInitialized()) {
-          return AutowareState::WaitingForRoute;
+          if (!flags_.waiting_after_initializing) {
+            flags_.waiting_after_initializing = true;
+            times_.initializing_completed = state_input_.current_time;
+            break;
+          }
+
+          // Wait after initialize completed to avoid sync error
+          constexpr double wait_time_after_initializing = 1.0;
+          const auto time_from_initializing = state_input_.current_time - times_.initializing_completed;
+          if (time_from_initializing.seconds() > wait_time_after_initializing) {
+            flags_.waiting_after_initializing = false;
+            return AutowareState::WaitingForRoute;
+          }
         }
 
         break;
@@ -232,8 +249,8 @@ AutowareState StateMachine::judgeAutowareState() const
         executing_route_ = state_input_.route;
 
         if (isPlanningCompleted()) {
-          if (!waiting_after_planning_) {
-            waiting_after_planning_ = true;
+          if (!flags_.waiting_after_planning) {
+            flags_.waiting_after_planning = true;
             times_.planning_completed = state_input_.current_time;
             break;
           }
@@ -242,7 +259,7 @@ AutowareState StateMachine::judgeAutowareState() const
           constexpr double wait_time_after_planning = 1.0;
           const auto time_from_planning = state_input_.current_time - times_.planning_completed;
           if (time_from_planning.seconds() > wait_time_after_planning) {
-            waiting_after_planning_ = false;
+            flags_.waiting_after_planning = false;
             return AutowareState::WaitingForEngage;
           }
         }
@@ -251,16 +268,8 @@ AutowareState StateMachine::judgeAutowareState() const
       }
 
     case (AutowareState::WaitingForEngage): {
-        if (isEmergency()) {
-          return AutowareState::Emergency;
-        }
-
         if (isRouteReceived()) {
           return AutowareState::Planning;
-        }
-
-        if (isEngaged()) {
-          return AutowareState::Driving;
         }
 
         if (hasArrivedGoal()) {
@@ -272,10 +281,6 @@ AutowareState StateMachine::judgeAutowareState() const
       }
 
     case (AutowareState::Driving): {
-        if (isEmergency()) {
-          return AutowareState::Emergency;
-        }
-
         if (isRouteReceived()) {
           return AutowareState::Planning;
         }
@@ -304,7 +309,7 @@ AutowareState StateMachine::judgeAutowareState() const
 
     case (AutowareState::Emergency): {
         if (!isEmergency()) {
-          return AutowareState::WaitingForEngage;
+          return state_before_emergency_;
         }
 
         break;