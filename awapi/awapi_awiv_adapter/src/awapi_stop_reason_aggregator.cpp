TakaHoribe/autoware.iv.universe

Feature/cut stop reason by dist (#859)
* cut far stop reason

* update readme

* remove &

* clean code

* change function name

* clean code

* use range-based for

* change type of idx (int -> size_t)

awapi/awapi_awiv_adapter/src/awapi_stop_reason_aggregator.cpp
@@ -18,22 +18,23 @@
 
 namespace autoware_api
 {
-AutowareIvStopReasonAggregator::AutowareIvStopReasonAggregator(const double timeout)
-: timeout_(timeout)
+AutowareIvStopReasonAggregator::AutowareIvStopReasonAggregator(
+  const double timeout, const double thresh_dist_to_stop_pose)
+: timeout_(timeout), thresh_dist_to_stop_pose_(thresh_dist_to_stop_pose)
 {
 }
 
 autoware_planning_msgs::StopReasonArray::ConstPtr
 AutowareIvStopReasonAggregator::updateStopReasonArray(
-  const autoware_planning_msgs::StopReasonArray::ConstPtr & msg_ptr)
+  const autoware_planning_msgs::StopReasonArray::ConstPtr & msg_ptr, const AutowareInfo & aw_info)
 {
-  applyUpdate(msg_ptr);
+  applyUpdate(msg_ptr, aw_info);
   applyTimeOut();
-  return makeStopReasonArray();
+  return makeStopReasonArray(aw_info);
 }
 
 void AutowareIvStopReasonAggregator::applyUpdate(
-  const autoware_planning_msgs::StopReasonArray::ConstPtr & msg_ptr)
+  const autoware_planning_msgs::StopReasonArray::ConstPtr & msg_ptr, const AutowareInfo & aw_info)
 {
   /* remove old stop_reason that matches reason with received msg */
   //make reason-matching msg list
@@ -93,19 +94,22 @@ void AutowareIvStopReasonAggregator::applyTimeOut()
 
 void AutowareIvStopReasonAggregator::appendStopReasonToArray(
   const autoware_planning_msgs::StopReason & stop_reason,
-  autoware_planning_msgs::StopReasonArray * stop_reason_array)
+  autoware_planning_msgs::StopReasonArray * stop_reason_array, const AutowareInfo & aw_info)
 {
+  // cut stop reason
+  const auto near_stop_reason = getNearStopReason(stop_reason, aw_info);
+
   //if stop factors is empty, not append
-  if (stop_reason.stop_factors.empty()) {
+  if (near_stop_reason.stop_factors.empty()) {
     return;
   }
 
   //if already exists same reason msg in stop_reason_array_msg, append stop_factors to there
-  for (int i = 0; i < stop_reason_array->stop_reasons.size(); i++) {
-    if (stop_reason_array->stop_reasons.at(i).reason == stop_reason.reason) {
-      stop_reason_array->stop_reasons.at(i).stop_factors.insert(
-        stop_reason_array->stop_reasons.at(i).stop_factors.end(), stop_reason.stop_factors.begin(),
-        stop_reason.stop_factors.end());
+  for (auto & base_stop_reasons : stop_reason_array->stop_reasons) {
+    if (base_stop_reasons.reason == near_stop_reason.reason) {
+      base_stop_reasons.stop_factors.insert(
+        base_stop_reasons.stop_factors.end(), near_stop_reason.stop_factors.begin(),
+        near_stop_reason.stop_factors.end());
       return;
     }
   }
@@ -115,7 +119,7 @@ void AutowareIvStopReasonAggregator::appendStopReasonToArray(
 }
 
 autoware_planning_msgs::StopReasonArray::ConstPtr
-AutowareIvStopReasonAggregator::makeStopReasonArray()
+AutowareIvStopReasonAggregator::makeStopReasonArray(const AutowareInfo & aw_info)
 {
   autoware_planning_msgs::StopReasonArray stop_reason_array_msg;
   // input header
@@ -125,10 +129,42 @@ AutowareIvStopReasonAggregator::makeStopReasonArray()
   // input stop reason
   for (const auto stop_reason_array : stop_reason_array_vec_) {
     for (const auto stop_reason : stop_reason_array.stop_reasons) {
-      appendStopReasonToArray(stop_reason, &stop_reason_array_msg);
+      appendStopReasonToArray(stop_reason, &stop_reason_array_msg, aw_info);
     }
   }
   return boost::make_shared<autoware_planning_msgs::StopReasonArray>(stop_reason_array_msg);
+}
+
+autoware_planning_msgs::StopReason AutowareIvStopReasonAggregator::getNearStopReason(
+  const autoware_planning_msgs::StopReason & stop_reason, const AutowareInfo & aw_info)
+{
+  if (!aw_info.autoware_planning_traj_ptr || !aw_info.current_pose_ptr) {
+    // pass through all stop reason
+    return stop_reason;
+  }
+
+  autoware_planning_msgs::StopReason near_stop_reason;
+  near_stop_reason.reason = stop_reason.reason;
+  for (const auto stop_factor : stop_reason.stop_factors) {
+    int self_idx;
+    int stop_idx;
+    if (
+      !planning_util::calcClosestIndex(
+        *aw_info.autoware_planning_traj_ptr, aw_info.current_pose_ptr->pose, self_idx) ||
+      !planning_util::calcClosestIndex(
+        *aw_info.autoware_planning_traj_ptr, stop_factor.stop_pose, stop_idx)) {
+      continue;
+    }
+    const double dist_to_stop_pose = planning_util::calcArcLengthFromWayPoint(
+      *aw_info.autoware_planning_traj_ptr, static_cast<size_t>(self_idx),
+      static_cast<size_t>(stop_idx));
+
+    if (dist_to_stop_pose < thresh_dist_to_stop_pose_) {
+      // append only near stop factor
+      near_stop_reason.stop_factors.emplace_back(stop_factor);
+    }
+  }
+  return near_stop_reason;
 }
 
 }  // namespace autoware_api

tier4/AutowareArchitectureProposal.iv

awapi/awapi_awiv_adapter/src/awapi_stop_reason_aggregator.cpp
@@ -18,24 +18,25 @@ namespace autoware_api
 {
 AutowareIvStopReasonAggregator::AutowareIvStopReasonAggregator(
   rclcpp::Node & node,
-  const double timeout)
+  const double timeout, const double thresh_dist_to_stop_pose)
 : logger_(node.get_logger().get_child("awapi_awiv_stop_reason_aggregator")),
   clock_(node.get_clock()),
-  timeout_(timeout)
+  timeout_(timeout),
+  thresh_dist_to_stop_pose_(thresh_dist_to_stop_pose)
 {
 }
 
 autoware_planning_msgs::msg::StopReasonArray::ConstSharedPtr
 AutowareIvStopReasonAggregator::updateStopReasonArray(
-  const autoware_planning_msgs::msg::StopReasonArray::ConstSharedPtr & msg_ptr)
+  const autoware_planning_msgs::msg::StopReasonArray::ConstSharedPtr & msg_ptr, const AutowareInfo & aw_info)
 {
-  applyUpdate(msg_ptr);
+  applyUpdate(msg_ptr, aw_info);
   applyTimeOut();
-  return makeStopReasonArray();
+  return makeStopReasonArray(aw_info);
 }
 
 void AutowareIvStopReasonAggregator::applyUpdate(
-  const autoware_planning_msgs::msg::StopReasonArray::ConstSharedPtr & msg_ptr)
+  const autoware_planning_msgs::msg::StopReasonArray::ConstSharedPtr & msg_ptr, const AutowareInfo & aw_info)
 {
   /* remove old stop_reason that matches reason with received msg */
   //make reason-matching msg list
@@ -98,19 +99,22 @@ void AutowareIvStopReasonAggregator::applyTimeOut()
 
 void AutowareIvStopReasonAggregator::appendStopReasonToArray(
   const autoware_planning_msgs::msg::StopReason & stop_reason,
-  autoware_planning_msgs::msg::StopReasonArray * stop_reason_array)
+  autoware_planning_msgs::msg::StopReasonArray * stop_reason_array, const AutowareInfo & aw_info)
 {
+  // cut stop reason
+  const auto near_stop_reason = getNearStopReason(stop_reason, aw_info);
+
   //if stop factors is empty, not append
-  if (stop_reason.stop_factors.empty()) {
+  if (near_stop_reason.stop_factors.empty()) {
     return;
   }
 
   //if already exists same reason msg in stop_reason_array_msg, append stop_factors to there
-  for (size_t i = 0; i < stop_reason_array->stop_reasons.size(); i++) {
-    if (stop_reason_array->stop_reasons.at(i).reason == stop_reason.reason) {
-      stop_reason_array->stop_reasons.at(i).stop_factors.insert(
-        stop_reason_array->stop_reasons.at(i).stop_factors.end(), stop_reason.stop_factors.begin(),
-        stop_reason.stop_factors.end());
+  for (auto & base_stop_reasons : stop_reason_array->stop_reasons) {
+    if (base_stop_reasons.reason == near_stop_reason.reason) {
+      base_stop_reasons.stop_factors.insert(
+        base_stop_reasons.stop_factors.end(), near_stop_reason.stop_factors.begin(),
+        near_stop_reason.stop_factors.end());
       return;
     }
   }
@@ -120,7 +124,7 @@ void AutowareIvStopReasonAggregator::appendStopReasonToArray(
 }
 
 autoware_planning_msgs::msg::StopReasonArray::ConstSharedPtr
-AutowareIvStopReasonAggregator::makeStopReasonArray()
+AutowareIvStopReasonAggregator::makeStopReasonArray(const AutowareInfo & aw_info)
 {
   autoware_planning_msgs::msg::StopReasonArray stop_reason_array_msg;
   // input header
@@ -130,10 +134,42 @@ AutowareIvStopReasonAggregator::makeStopReasonArray()
   // input stop reason
   for (const auto stop_reason_array : stop_reason_array_vec_) {
     for (const auto stop_reason : stop_reason_array.stop_reasons) {
-      appendStopReasonToArray(stop_reason, &stop_reason_array_msg);
+      appendStopReasonToArray(stop_reason, &stop_reason_array_msg, aw_info);
     }
   }
   return std::make_shared<autoware_planning_msgs::msg::StopReasonArray>(stop_reason_array_msg);
+}
+
+autoware_planning_msgs::msg::StopReason AutowareIvStopReasonAggregator::getNearStopReason(
+  const autoware_planning_msgs::msg::StopReason & stop_reason, const AutowareInfo & aw_info)
+{
+  if (!aw_info.autoware_planning_traj_ptr || !aw_info.current_pose_ptr) {
+    // pass through all stop reason
+    return stop_reason;
+  }
+
+  autoware_planning_msgs::msg::StopReason near_stop_reason;
+  near_stop_reason.reason = stop_reason.reason;
+  for (const auto stop_factor : stop_reason.stop_factors) {
+    int self_idx;
+    int stop_idx;
+    if (
+      !planning_util::calcClosestIndex(
+        *aw_info.autoware_planning_traj_ptr, aw_info.current_pose_ptr->pose, self_idx) ||
+      !planning_util::calcClosestIndex(
+        *aw_info.autoware_planning_traj_ptr, stop_factor.stop_pose, stop_idx)) {
+      continue;
+    }
+    const double dist_to_stop_pose = planning_util::calcArcLengthFromWayPoint(
+      *aw_info.autoware_planning_traj_ptr, static_cast<size_t>(self_idx),
+      static_cast<size_t>(stop_idx));
+
+    if (dist_to_stop_pose < thresh_dist_to_stop_pose_) {
+      // append only near stop factor
+      near_stop_reason.stop_factors.emplace_back(stop_factor);
+    }
+  }
+  return near_stop_reason;
 }
 
 }  // namespace autoware_api