7a6bb464759fb763e6e3cb1daee2385f6ba97725
Shigeki Kobayashi <shigeki.kobayashi@tier4.jp>
2021-02-05 18:03:07 +0900

    fix bug

--- tmp/trajectory_loader/src/main.cpp
+++ tmp/trajectory_loader/src/main.cpp
@@ -4,10 +4,7 @@
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
-  TrajectoryLoaderNode node;
-
   rclcpp::spin(std::make_shared<TrajectoryLoaderNode>());
   rclcpp::shutdown();
-
   return 0;
 }
--- tmp/trajectory_loader/src/node.cpp
+++ tmp/trajectory_loader/src/node.cpp
@@ -5,22 +5,12 @@
 
 TrajectoryLoaderNode::TrajectoryLoaderNode() : Node("trajectory_loader_node")
 {
-  rclcpp::Publisher<autoware_planning_msgs::msg::Trajectory>::SharedPtr pub_;
-  //todo remove rcl
-  // QoS setup
-  static constexpr std::size_t queue_size = 1;
-  rclcpp::QoS durable_qos(queue_size);
-  durable_qos.transient_local();  // option for latching
-
   pub_ = this->create_publisher<autoware_planning_msgs::msg::Trajectory>("trajectory", 1);
 
   // param
   std::string file_name, frame_id;
 
-  //pnh_.getParam("trajectory_file_name", file_name);
-  //nh_.param("frame_id", frame_id, std::string("map"));
   file_name = this->declare_parameter("trajectory_file_name", std::string("trajectory_file_name"));
-  RCLCPP_INFO(get_logger(), "file name is %s", file_name.c_str());
   frame_id = this->declare_parameter("frame_id", std::string("map"));
 
   csv file_data;
@@ -28,11 +18,8 @@ TrajectoryLoaderNode::TrajectoryLoaderNode() : Node("trajectory_loader_node")
 
   // load csv file
   rclcpp::Rate loop_rate(1);
-  int c = 0;
   while (!loadData(file_name, label_row_association_map, file_data) && rclcpp::ok()) {
     loop_rate.sleep();
-    c++;
-    RCLCPP_INFO(get_logger(), "c = %i", c);
   }
 
   // publish
@@ -53,17 +40,12 @@ bool TrajectoryLoaderNode::publish(
     double roll, pitch, yaw;
 
     for (size_t j = 0; j < file_data.at(i).size(); ++j) {
-      RCLCPP_INFO(get_logger(), "i = %zu, j = %zu", i, j);
       if (label_row_association_map.at("x") == (int)j) {
         point.pose.position.x = std::stof(file_data.at(i).at(j));
       } else if (label_row_association_map.at("y") == (int)j) {
         point.pose.position.y = std::stof(file_data.at(i).at(j));
       } else if (label_row_association_map.at("z") == (int)j) {
         point.pose.position.z = std::stof(file_data.at(i).at(j));
-      } else if (label_row_association_map.at("roll") == (int)j) {
-        roll = std::stof(file_data.at(i).at(j));
-      } else if (label_row_association_map.at("pitch") == (int)j) {
-        pitch = std::stof(file_data.at(i).at(j));
       } else if (label_row_association_map.at("yaw") == (int)j) {
         yaw = std::stof(file_data.at(i).at(j));
       } else if (label_row_association_map.at("linear_velocity") == (int)j) {
@@ -76,12 +58,13 @@ bool TrajectoryLoaderNode::publish(
         point.accel.angular.z = std::stof(file_data.at(i).at(j));
       }
     }
+    roll = 0;
+    pitch = 0;
     tf2::Quaternion tf2_quaternion;
     tf2_quaternion.setRPY(roll, pitch, yaw);
     tf2::convert(point.pose.orientation, tf2_quaternion);
     msg.points.push_back(point);
   }
-
   pub_->publish(msg);
   return true;
 }
@@ -112,9 +95,6 @@ bool TrajectoryLoaderNode::loadData(
       deleteUnit(str_vec.at(i));
       deleteHeadSpace(str_vec.at(i));
       label_row_association_map[str_vec.at(i)] = (int)i;
-      RCLCPP_INFO(
-        get_logger(), "label_row_association_map[str_vec.at(i)] = %i, str_vec.at(i) = %s",
-        label_row_association_map[str_vec.at(i)], str_vec.at(i).c_str());
     }
   } else {
     RCLCPP_ERROR(get_logger(), "cannot create association map");
