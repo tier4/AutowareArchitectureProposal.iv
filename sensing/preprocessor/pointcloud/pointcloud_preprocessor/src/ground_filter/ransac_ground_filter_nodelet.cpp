TakaHoribe/autoware.iv.universe

ransac with voxel grid (#1110)
* add voxel downsample filter

* add voxel size param

* update license

* remove color map

* update comment

* remove unnecessary license

* remove unnecessary include

* change to range-based-for

sensing/preprocessor/pointcloud/pointcloud_preprocessor/src/ground_filter/ransac_ground_filter_nodelet.cpp
@@ -13,66 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/*
- * Copyright 2017-2019 Autoware Foundation. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ********************
- *  v1.0: amc-nu (abrahammonrroy@yahoo.com)
- */
-/*********************************************************************
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2014, JSK Lab
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/o2r other materials provided
- *     with the distribution.
- *   * Neither the name of the JSK Lab nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *********************************************************************/
-
-#include "pointcloud_preprocessor/ground_filter/ransac_ground_filter_nodelet.h"
 
 #include <pluginlib/class_list_macros.h>
-
+#include "pointcloud_preprocessor/ground_filter/ransac_ground_filter_nodelet.h"
 #include <pcl/common/centroid.h>
-#include <pcl/visualization/common/common.h>
 #include <pcl_ros/transforms.h>
-
-#include <eigen_conversions/eigen_msg.h>
 #include <random>
 
 namespace
@@ -115,20 +60,6 @@ pointcloud_preprocessor::PlaneBasis getPlaneBasis(const Eigen::Vector3d & plane_
   return basis;
 }
 
-pointcloud_preprocessor::RGB getRandColor()
-{
-  pointcloud_preprocessor::RGB color;
-  std::random_device rnd;
-  std::mt19937 mt(rnd());
-  std::uniform_int_distribution<> rrand(0, 255);
-  color.r = rrand(mt);
-  std::uniform_int_distribution<> grand(0, 255);
-  color.g = grand(mt);
-  std::uniform_int_distribution<> brand(0, 255);
-  color.b = brand(mt);
-  return color;
-}
-
 geometry_msgs::Pose getDebugPose(const Eigen::Affine3d & plane_affine)
 {
   geometry_msgs::Pose debug_pose;
@@ -158,25 +89,20 @@ void RANSACGroundFilterNodelet::setDebugPublisher()
 
 void RANSACGroundFilterNodelet::publishDebugMessage(
   const geometry_msgs::PoseArray & debug_pose_array,
-  const std::vector<pcl::PointCloud<PointType>> & ground_clouds, const std_msgs::Header & header)
+  const pcl::PointCloud<PointType> & ground_cloud, const std_msgs::Header & header)
 {
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr colored_ground_ptr(new pcl::PointCloud<pcl::PointXYZRGB>);
-  for (int i = 0; i < ground_clouds.size(); ++i) {
-    RGB color = getRandColor();
-    if (i < color_map_.size()) {
-      color = color_map_.at(i);
-    }
-    for (int j = 0; j < ground_clouds[i].points.size(); ++j) {
-      pcl::PointXYZRGB p;
-      p.x = ground_clouds[i].points[j].x;
-      p.y = ground_clouds[i].points[j].y;
-      p.z = ground_clouds[i].points[j].z;
-      p.r = color.r;
-      p.g = color.g;
-      p.b = color.b;
-      colored_ground_ptr->points.push_back(p);
-    }
+  for (const auto & ground_point : ground_cloud.points) {
+    pcl::PointXYZRGB p;
+    p.x = ground_point.x;
+    p.y = ground_point.y;
+    p.z = ground_point.z;
+    p.r = 255.0;
+    p.g = 0.0;
+    p.b = 0.0;
+    colored_ground_ptr->points.push_back(p);
   }
+
   sensor_msgs::PointCloud2::Ptr ground_cloud_msg_ptr(new sensor_msgs::PointCloud2);
   ground_cloud_msg_ptr->header = header;
   pcl::toROSMsg(*colored_ground_ptr, *ground_cloud_msg_ptr);
@@ -256,58 +182,27 @@ Eigen::Affine3d RANSACGroundFilterNodelet::getPlaneAffine(
   return trans * rot;
 }
 
-void RANSACGroundFilterNodelet::applyRecursiveRANSAC(
+void RANSACGroundFilterNodelet::applyRANSAC(
   const pcl::PointCloud<PointType>::Ptr & input,
-  std::vector<pcl::PointIndices::Ptr> & output_inliers,
-  std::vector<pcl::ModelCoefficients::Ptr> & output_coefficients,
-  pcl::PointCloud<PointType>::Ptr & rest_cloud)
+  pcl::PointIndices::Ptr & output_inliers,
+  pcl::ModelCoefficients::Ptr & output_coefficients)
 {
-  *rest_cloud = *input;
-  int counter = 0;
-  while (true) {
-    ++counter;
-    pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
-    pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
-
-    pcl::SACSegmentation<PointType> seg;
-    seg.setOptimizeCoefficients(true);
-    seg.setRadiusLimits(0.3, std::numeric_limits<double>::max());
-    seg.setMethodType(pcl::SAC_RANSAC);
-    seg.setDistanceThreshold(outlier_threshold_);
-    seg.setInputCloud(rest_cloud);
-    seg.setMaxIterations(max_iterations_);
-    seg.setModelType(pcl::SACMODEL_PLANE);
-    seg.segment(*inliers, *coefficients);
-
-    if (inliers->indices.size() >= min_inliers_) {
-      output_inliers.push_back(inliers);
-      output_coefficients.push_back(coefficients);
-    } else {
-      if (min_trial_ <= counter) {
-        return;
-      }
-    }
-
-    // prepare for next loop
-    pcl::PointCloud<PointType>::Ptr next_rest_cloud(new pcl::PointCloud<PointType>);
-    pcl::ExtractIndices<PointType> ex;
-    ex.setInputCloud(rest_cloud);
-    ex.setIndices(inliers);
-    ex.setNegative(true);
-    ex.setKeepOrganized(true);
-    ex.filter(*next_rest_cloud);
-    if (next_rest_cloud->points.size() < min_points_) {
-      NODELET_DEBUG("no more enough points are left");
-      return;
-    }
-    rest_cloud = next_rest_cloud;
-  }
+  pcl::SACSegmentation<PointType> seg;
+  seg.setOptimizeCoefficients(true);
+  seg.setRadiusLimits(0.3, std::numeric_limits<double>::max());
+  seg.setMethodType(pcl::SAC_RANSAC);
+  seg.setDistanceThreshold(outlier_threshold_);
+  seg.setInputCloud(input);
+  seg.setMaxIterations(max_iterations_);
+  seg.setModelType(pcl::SACMODEL_PLANE);
+  seg.segment(*output_inliers, *output_coefficients);
 }
 
 void RANSACGroundFilterNodelet::filter(
   const PointCloud2::ConstPtr & input, const IndicesPtr & indices, PointCloud2 & output)
 {
   boost::mutex::scoped_lock lock(mutex_);
+
   sensor_msgs::PointCloud2::Ptr input_transed_ptr(new sensor_msgs::PointCloud2);
   if (!transformPointCloud(base_frame_, input, input_transed_ptr)) {
     ROS_ERROR_STREAM_THROTTLE(
@@ -317,43 +212,46 @@ void RANSACGroundFilterNodelet::filter(
   pcl::PointCloud<PointType>::Ptr current_sensor_cloud_ptr(new pcl::PointCloud<PointType>);
   pcl::fromROSMsg(*input_transed_ptr, *current_sensor_cloud_ptr);
 
-  std::vector<pcl::PointIndices::Ptr> inliers_vector;
-  std::vector<pcl::ModelCoefficients::Ptr> coefficients;
-  pcl::PointCloud<PointType>::Ptr rest_cloud_ptr(new pcl::PointCloud<PointType>);
-  applyRecursiveRANSAC(current_sensor_cloud_ptr, inliers_vector, coefficients, rest_cloud_ptr);
+  // downsample pointcloud to reduce ransac calculation cost
+  pcl::PointCloud<PointType>::Ptr downsampled_cloud(new pcl::PointCloud<PointType>);
+  downsampled_cloud->points.reserve(current_sensor_cloud_ptr->points.size());
+  pcl::VoxelGrid<PointType> filter;
+  filter.setInputCloud(current_sensor_cloud_ptr);
+  filter.setLeafSize(voxel_size_x_, voxel_size_y_, voxel_size_z_);
+  filter.filter(*downsampled_cloud);
 
-  geometry_msgs::PoseArray debug_pose_array;
-  debug_pose_array.header.frame_id = base_frame_;
-  std::vector<pcl::PointCloud<PointType>> debug_ground_clouds;
-  pcl::PointCloud<PointType>::Ptr no_ground_cloud_ptr(new pcl::PointCloud<PointType>);
-  for (int i = 0; i < inliers_vector.size(); ++i) {
-    Eigen::Vector3d plane_normal(
-      coefficients.at(i)->values[0], coefficients.at(i)->values[1], coefficients.at(i)->values[2]);
-    {
-      const auto plane_slope = std::abs(
-        std::acos(plane_normal.dot(unit_vec_) / (plane_normal.norm() * unit_vec_.norm())) * 180 /
-        M_PI);
-      if (plane_slope > plane_slope_threshold_) {
-        continue;
-      }
+  // apply ransac
+  pcl::PointIndices::Ptr inliers(new pcl::PointIndices);;
+  pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+  applyRANSAC(downsampled_cloud, inliers, coefficients);
+
+  // filter too tilt plane to avoid mis-fitting (e.g. fitting to wall plane)
+  Eigen::Vector3d plane_normal(
+    coefficients->values[0], coefficients->values[1], coefficients->values[2]);
+  {
+    const auto plane_slope = std::abs(
+      std::acos(plane_normal.dot(unit_vec_) / (plane_normal.norm() * unit_vec_.norm())) * 180 /
+      M_PI);
+    if (plane_slope > plane_slope_threshold_) {
+      return;
     }
+  }
 
-    pcl::PointCloud<PointType>::Ptr segment_ground_cloud_ptr(new pcl::PointCloud<PointType>);
-    pcl::PointCloud<PointType>::Ptr segment_no_ground_cloud_ptr(new pcl::PointCloud<PointType>);
-    extractPointsIndices(
-      current_sensor_cloud_ptr, *inliers_vector[i], segment_ground_cloud_ptr,
-      segment_no_ground_cloud_ptr);
-    const Eigen::Affine3d plane_affine = getPlaneAffine(*segment_ground_cloud_ptr, plane_normal);
-    const geometry_msgs::Pose debug_pose = getDebugPose(plane_affine);
-    debug_pose_array.poses.push_back(debug_pose);
-    debug_ground_clouds.push_back(*segment_ground_cloud_ptr);
+  // extract pointcloud from indices
+  pcl::PointCloud<PointType>::Ptr segment_ground_cloud_ptr(new pcl::PointCloud<PointType>);
+  pcl::PointCloud<PointType>::Ptr segment_no_ground_cloud_ptr(new pcl::PointCloud<PointType>);
+  extractPointsIndices(
+    downsampled_cloud, *inliers, segment_ground_cloud_ptr,
+    segment_no_ground_cloud_ptr);
+  const Eigen::Affine3d plane_affine = getPlaneAffine(*segment_ground_cloud_ptr, plane_normal);
+  pcl::PointCloud<PointType>::Ptr no_ground_cloud_ptr(new pcl::PointCloud<PointType>);
 
-    for (const auto & p : rest_cloud_ptr->points) {
-      const Eigen::Vector3d transformed_point =
-        plane_affine.inverse() * Eigen::Vector3d(p.x, p.y, p.z);
-      if (std::abs(transformed_point.z()) > height_threshold_) {
-        no_ground_cloud_ptr->points.push_back(p);
-      }
+  // use not downsamped pointcloud for extract pointcloud that higher than height threshold
+  for (const auto & p : current_sensor_cloud_ptr->points) {
+    const Eigen::Vector3d transformed_point =
+      plane_affine.inverse() * Eigen::Vector3d(p.x, p.y, p.z);
+    if (std::abs(transformed_point.z()) > height_threshold_) {
+      no_ground_cloud_ptr->points.push_back(p);
     }
   }
 
@@ -369,8 +267,13 @@ void RANSACGroundFilterNodelet::filter(
   }
   output = *no_ground_cloud_transed_msg_ptr;
 
+  // output debug plane coords and ground pointcloud when debug flag is set
   if (debug_) {
-    publishDebugMessage(debug_pose_array, debug_ground_clouds, input->header);
+    const geometry_msgs::Pose debug_pose = getDebugPose(plane_affine);
+    geometry_msgs::PoseArray debug_pose_array;
+    debug_pose_array.header.frame_id = base_frame_;
+    debug_pose_array.poses.push_back(debug_pose);
+    publishDebugMessage(debug_pose_array, *segment_ground_cloud_ptr, input->header);
   }
 }
 
@@ -388,12 +291,6 @@ void RANSACGroundFilterNodelet::config_callback(
     NODELET_DEBUG(
       "[%s::config_callback] Setting base_frame to: %s.", getName().c_str(),
       config.base_frame.c_str());
-  }
-
-  if (min_trial_ != config.min_trial) {
-    min_trial_ = config.min_trial;
-    NODELET_DEBUG(
-      "[%s::config_callback] Setting min_trial to: %d.", getName().c_str(), config.min_trial);
   }
 
   if (max_iterations_ != config.max_iterations) {
@@ -434,6 +331,27 @@ void RANSACGroundFilterNodelet::config_callback(
     NODELET_DEBUG(
       "[%s::config_callback] Setting plane_slope_threshold to: %lf.", getName().c_str(),
       config.plane_slope_threshold);
+  }
+
+  if (voxel_size_x_ != config.voxel_size_x) {
+    voxel_size_x_ = config.voxel_size_x;
+    NODELET_DEBUG(
+      "[%s::config_callback] Setting voxel_size_x to: %lf.", getName().c_str(),
+      config.voxel_size_x);
+  }
+
+  if (voxel_size_y_ != config.voxel_size_y) {
+    voxel_size_y_ = config.voxel_size_y;
+    NODELET_DEBUG(
+      "[%s::config_callback] Setting voxel_size_y to: %lf.", getName().c_str(),
+      config.voxel_size_y);
+  }
+
+  if (voxel_size_z_ != config.voxel_size_z) {
+    voxel_size_z_ = config.voxel_size_z;
+    NODELET_DEBUG(
+      "[%s::config_callback] Setting voxel_size_z to: %lf.", getName().c_str(),
+      config.voxel_size_z);
   }
 
   if (debug_ != config.debug) {

tier4/AutowareArchitectureProposal.iv

sensing/preprocessor/pointcloud/pointcloud_preprocessor/src/ground_filter/ransac_ground_filter_nodelet.cpp
@@ -13,56 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/*
- * Copyright 2017-2019 Autoware Foundation. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ********************
- *  v1.0: amc-nu (abrahammonrroy@yahoo.com)
- */
-/*********************************************************************
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2014, JSK Lab
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/o2r other materials provided
- *     with the distribution.
- *   * Neither the name of the JSK Lab nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *********************************************************************/
 
 #include "pointcloud_preprocessor/ground_filter/ransac_ground_filter_nodelet.hpp"
 
@@ -113,20 +63,6 @@ pointcloud_preprocessor::PlaneBasis getPlaneBasis(const Eigen::Vector3d & plane_
   return basis;
 }
 
-pointcloud_preprocessor::RGB getRandColor()
-{
-  pointcloud_preprocessor::RGB color;
-  std::random_device rnd;
-  std::mt19937 mt(rnd());
-  std::uniform_int_distribution<> rrand(0, 255);
-  color.r = rrand(mt);
-  std::uniform_int_distribution<> grand(0, 255);
-  color.g = grand(mt);
-  std::uniform_int_distribution<> brand(0, 255);
-  color.b = brand(mt);
-  return color;
-}
-
 geometry_msgs::msg::Pose getDebugPose(const Eigen::Affine3d & plane_affine)
 {
   geometry_msgs::msg::Pose debug_pose;
@@ -149,12 +85,14 @@ RANSACGroundFilterComponent::RANSACGroundFilterComponent(const rclcpp::NodeOptio
 {
   base_frame_ = declare_parameter("base_frame", "base_link");
   unit_axis_ = declare_parameter("unit_axis", "z");
-  min_trial_ = declare_parameter("min_trial", 0);
   max_iterations_ = declare_parameter("max_iterations", 1000);
   min_inliers_ = declare_parameter("min_trial", 5000);
   min_points_ = declare_parameter("min_points", 1000);
   outlier_threshold_ = declare_parameter("outlier_threshold", 0.01);
   plane_slope_threshold_ = declare_parameter("plane_slope_threshold", 10.0);
+  voxel_size_x_ = declare_parameter("voxel_size_x", 0.04);
+  voxel_size_y_ = declare_parameter("voxel_size_y", 0.04);
+  voxel_size_z_ = declare_parameter("voxel_size_z", 0.04);
   height_threshold_ = declare_parameter("height_threshold", 0.01);
   debug_ = declare_parameter("debug", false);
 
@@ -187,24 +125,18 @@ void RANSACGroundFilterComponent::setDebugPublisher()
 
 void RANSACGroundFilterComponent::publishDebugMessage(
   const geometry_msgs::msg::PoseArray & debug_pose_array,
-  const std::vector<pcl::PointCloud<PointType>> & ground_clouds, const std_msgs::msg::Header & header)
+  const pcl::PointCloud<PointType> & ground_cloud, const std_msgs::msg::Header & header)
 {
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr colored_ground_ptr(new pcl::PointCloud<pcl::PointXYZRGB>);
-  for (int i = 0; i < ground_clouds.size(); ++i) {
-    RGB color = getRandColor();
-    if (i < color_map_.size()) {
-      color = color_map_.at(i);
-    }
-    for (int j = 0; j < ground_clouds[i].points.size(); ++j) {
-      pcl::PointXYZRGB p;
-      p.x = ground_clouds[i].points[j].x;
-      p.y = ground_clouds[i].points[j].y;
-      p.z = ground_clouds[i].points[j].z;
-      p.r = color.r;
-      p.g = color.g;
-      p.b = color.b;
-      colored_ground_ptr->points.push_back(p);
-    }
+  for (const auto & ground_point : ground_cloud.points) {
+    pcl::PointXYZRGB p;
+    p.x = ground_point.x;
+    p.y = ground_point.y;
+    p.z = ground_point.z;
+    p.r = 255.0;
+    p.g = 0.0;
+    p.b = 0.0;
+    colored_ground_ptr->points.push_back(p);
   }
   sensor_msgs::msg::PointCloud2::SharedPtr ground_cloud_msg_ptr(new sensor_msgs::msg::PointCloud2);
   ground_cloud_msg_ptr->header = header;
@@ -272,52 +204,20 @@ Eigen::Affine3d RANSACGroundFilterComponent::getPlaneAffine(
   return trans * rot;
 }
 
-void RANSACGroundFilterComponent::applyRecursiveRANSAC(
+void RANSACGroundFilterComponent::applyRANSAC(
   const pcl::PointCloud<PointType>::Ptr & input,
-  std::vector<pcl::PointIndices::Ptr> & output_inliers,
-  std::vector<pcl::ModelCoefficients::Ptr> & output_coefficients,
-  pcl::PointCloud<PointType>::Ptr & rest_cloud)
+  pcl::PointIndices::Ptr & output_inliers,
+  pcl::ModelCoefficients::Ptr & output_coefficients)
 {
-  *rest_cloud = *input;
-  int counter = 0;
-  while (true) {
-    ++counter;
-    pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
-    pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
-
-    pcl::SACSegmentation<PointType> seg;
-    seg.setOptimizeCoefficients(true);
-    seg.setRadiusLimits(0.3, std::numeric_limits<double>::max());
-    seg.setMethodType(pcl::SAC_RANSAC);
-    seg.setDistanceThreshold(outlier_threshold_);
-    seg.setInputCloud(rest_cloud);
-    seg.setMaxIterations(max_iterations_);
-    seg.setModelType(pcl::SACMODEL_PLANE);
-    seg.segment(*inliers, *coefficients);
-
-    if (inliers->indices.size() >= min_inliers_) {
-      output_inliers.push_back(inliers);
-      output_coefficients.push_back(coefficients);
-    } else {
-      if (min_trial_ <= counter) {
-        return;
-      }
-    }
-
-    // prepare for next loop
-    pcl::PointCloud<PointType>::Ptr next_rest_cloud(new pcl::PointCloud<PointType>);
-    pcl::ExtractIndices<PointType> ex;
-    ex.setInputCloud(rest_cloud);
-    ex.setIndices(inliers);
-    ex.setNegative(true);
-    ex.setKeepOrganized(true);
-    ex.filter(*next_rest_cloud);
-    if (next_rest_cloud->points.size() < min_points_) {
-      RCLCPP_DEBUG(this->get_logger(), "no more enough points are left");
-      return;
-    }
-    rest_cloud = next_rest_cloud;
-  }
+  pcl::SACSegmentation<PointType> seg;
+  seg.setOptimizeCoefficients(true);
+  seg.setRadiusLimits(0.3, std::numeric_limits<double>::max());
+  seg.setMethodType(pcl::SAC_RANSAC);
+  seg.setDistanceThreshold(outlier_threshold_);
+  seg.setInputCloud(input);
+  seg.setMaxIterations(max_iterations_);
+  seg.setModelType(pcl::SACMODEL_PLANE);
+  seg.segment(*output_inliers, *output_coefficients);
 }
 
 void RANSACGroundFilterComponent::filter(
@@ -334,43 +234,46 @@ void RANSACGroundFilterComponent::filter(
   pcl::PointCloud<PointType>::Ptr current_sensor_cloud_ptr(new pcl::PointCloud<PointType>);
   pcl::fromROSMsg(*input_transed_ptr, *current_sensor_cloud_ptr);
 
-  std::vector<pcl::PointIndices::Ptr> inliers_vector;
-  std::vector<pcl::ModelCoefficients::Ptr> coefficients;
-  pcl::PointCloud<PointType>::Ptr rest_cloud_ptr(new pcl::PointCloud<PointType>);
-  applyRecursiveRANSAC(current_sensor_cloud_ptr, inliers_vector, coefficients, rest_cloud_ptr);
+  // downsample pointcloud to reduce ransac calculation cost
+  pcl::PointCloud<PointType>::Ptr downsampled_cloud(new pcl::PointCloud<PointType>);
+  downsampled_cloud->points.reserve(current_sensor_cloud_ptr->points.size());
+  pcl::VoxelGrid<PointType> filter;
+  filter.setInputCloud(current_sensor_cloud_ptr);
+  filter.setLeafSize(voxel_size_x_, voxel_size_y_, voxel_size_z_);
+  filter.filter(*downsampled_cloud);
 
-  geometry_msgs::msg::PoseArray debug_pose_array;
-  debug_pose_array.header.frame_id = base_frame_;
-  std::vector<pcl::PointCloud<PointType>> debug_ground_clouds;
-  pcl::PointCloud<PointType>::Ptr no_ground_cloud_ptr(new pcl::PointCloud<PointType>);
-  for (int i = 0; i < inliers_vector.size(); ++i) {
-    Eigen::Vector3d plane_normal(
-      coefficients.at(i)->values[0], coefficients.at(i)->values[1], coefficients.at(i)->values[2]);
-    {
-      const auto plane_slope = std::abs(
-        std::acos(plane_normal.dot(unit_vec_) / (plane_normal.norm() * unit_vec_.norm())) * 180 /
-        M_PI);
-      if (plane_slope > plane_slope_threshold_) {
-        continue;
-      }
+  // apply ransac
+  pcl::PointIndices::Ptr inliers(new pcl::PointIndices);;
+  pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+  applyRANSAC(downsampled_cloud, inliers, coefficients);
+
+  // filter too tilt plane to avoid mis-fitting (e.g. fitting to wall plane)
+  Eigen::Vector3d plane_normal(
+    coefficients->values[0], coefficients->values[1], coefficients->values[2]);
+  {
+    const auto plane_slope = std::abs(
+      std::acos(plane_normal.dot(unit_vec_) / (plane_normal.norm() * unit_vec_.norm())) * 180 /
+      M_PI);
+    if (plane_slope > plane_slope_threshold_) {
+      return;
     }
+  }
 
-    pcl::PointCloud<PointType>::Ptr segment_ground_cloud_ptr(new pcl::PointCloud<PointType>);
-    pcl::PointCloud<PointType>::Ptr segment_no_ground_cloud_ptr(new pcl::PointCloud<PointType>);
-    extractPointsIndices(
-      current_sensor_cloud_ptr, *inliers_vector[i], segment_ground_cloud_ptr,
-      segment_no_ground_cloud_ptr);
-    const Eigen::Affine3d plane_affine = getPlaneAffine(*segment_ground_cloud_ptr, plane_normal);
-    const geometry_msgs::msg::Pose debug_pose = getDebugPose(plane_affine);
-    debug_pose_array.poses.push_back(debug_pose);
-    debug_ground_clouds.push_back(*segment_ground_cloud_ptr);
+  // extract pointcloud from indices
+  pcl::PointCloud<PointType>::Ptr segment_ground_cloud_ptr(new pcl::PointCloud<PointType>);
+  pcl::PointCloud<PointType>::Ptr segment_no_ground_cloud_ptr(new pcl::PointCloud<PointType>);
+  extractPointsIndices(
+    downsampled_cloud, *inliers, segment_ground_cloud_ptr,
+    segment_no_ground_cloud_ptr);
+  const Eigen::Affine3d plane_affine = getPlaneAffine(*segment_ground_cloud_ptr, plane_normal);
+  pcl::PointCloud<PointType>::Ptr no_ground_cloud_ptr(new pcl::PointCloud<PointType>);
 
-    for (const auto & p : rest_cloud_ptr->points) {
-      const Eigen::Vector3d transformed_point =
-        plane_affine.inverse() * Eigen::Vector3d(p.x, p.y, p.z);
-      if (std::abs(transformed_point.z()) > height_threshold_) {
-        no_ground_cloud_ptr->points.push_back(p);
-      }
+  // use not downsamped pointcloud for extract pointcloud that higher than height threshold
+  for (const auto & p : current_sensor_cloud_ptr->points) {
+    const Eigen::Vector3d transformed_point =
+      plane_affine.inverse() * Eigen::Vector3d(p.x, p.y, p.z);
+    if (std::abs(transformed_point.z()) > height_threshold_) {
+      no_ground_cloud_ptr->points.push_back(p);
     }
   }
 
@@ -387,8 +290,13 @@ void RANSACGroundFilterComponent::filter(
   }
   output = *no_ground_cloud_transed_msg_ptr;
 
+  // output debug plane coords and ground pointcloud when debug flag is set
   if (debug_) {
-    publishDebugMessage(debug_pose_array, debug_ground_clouds, input->header);
+    const geometry_msgs::msg::Pose debug_pose = getDebugPose(plane_affine);
+    geometry_msgs::msg::PoseArray debug_pose_array;
+    debug_pose_array.header.frame_id = base_frame_;
+    debug_pose_array.poses.push_back(debug_pose);
+    publishDebugMessage(debug_pose_array, *segment_ground_cloud_ptr, input->header);
   }
 }
 
@@ -399,9 +307,6 @@ rcl_interfaces::msg::SetParametersResult RANSACGroundFilterComponent::paramCallb
 
   if (get_param(p, "base_frame", base_frame_)) {
     RCLCPP_DEBUG(get_logger(), "Setting base_frame to: %s.", base_frame_.c_str());
-  }
-  if (get_param(p, "min_trial", min_trial_)) {
-    RCLCPP_DEBUG(get_logger(), "Setting min_trial to: %d.", min_trial_);
   }
   if (get_param(p, "max_iterations", max_iterations_)) {
     RCLCPP_DEBUG(get_logger(), "Setting max_iterations to: %d.", max_iterations_);
@@ -422,6 +327,18 @@ rcl_interfaces::msg::SetParametersResult RANSACGroundFilterComponent::paramCallb
   if (get_param(p, "plane_slope_threshold", plane_slope_threshold_)) {
     RCLCPP_DEBUG(
       get_logger(), "Setting plane_slope_threshold to: %lf.", plane_slope_threshold_);
+  }
+  if (get_param(p, "voxel_size_x", voxel_size_x_)) {
+    RCLCPP_DEBUG(
+      get_logger(), "Setting voxel_size_x to: %lf.", voxel_size_x_);
+  }
+  if (get_param(p, "voxel_size_y", voxel_size_y_)) {
+    RCLCPP_DEBUG(
+      get_logger(), "Setting voxel_size_y to: %lf.", voxel_size_y_);
+  }
+  if (get_param(p, "voxel_size_z", voxel_size_z_)) {
+    RCLCPP_DEBUG(
+      get_logger(), "Setting voxel_size_z to: %lf.", voxel_size_z_);
   }
   if (get_param(p, "debug", debug_)) {
     RCLCPP_DEBUG(