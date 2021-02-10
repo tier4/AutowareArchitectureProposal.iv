b217e066734b43bbcc80c56af89a2b767576b339
taikitanaka <ttatcoder@outlook.jp>
2021-02-10 10:05:53 +0900

    refactor for ros2 style

--- perception/traffic_light_recognition/traffic_light_ssd_fine_detector/include/traffic_light_ssd_fine_detector/nodelet.hpp
+++ perception/traffic_light_recognition/traffic_light_ssd_fine_detector/include/traffic_light_ssd_fine_detector/nodelet.hpp
@@ -20,9 +20,8 @@
 #include <mutex>
 #include <string>
 
-#include <opencv2/opencv.hpp>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
 
 #include "trt_ssd.hpp"
 
