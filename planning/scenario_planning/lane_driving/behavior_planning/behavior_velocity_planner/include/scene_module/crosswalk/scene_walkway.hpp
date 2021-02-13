240da460d417d0aefec2434d0b26c40fb1f55ad8
taikitanaka <ttatcoder@outlook.jp>
2021-02-14 08:59:04 +0900

    fixe commit

--- planning/scenario_planning/lane_driving/behavior_planning/behavior_velocity_planner/include/scene_module/crosswalk/scene_walkway.hpp
+++ planning/scenario_planning/lane_driving/behavior_planning/behavior_velocity_planner/include/scene_module/crosswalk/scene_walkway.hpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#define SCENE_MODULE__CROSSWALK__SCENE_WALKWAY_HPP_
 #ifndef SCENE_MODULE__CROSSWALK__SCENE_WALKWAY_HPP_
+#define SCENE_MODULE__CROSSWALK__SCENE_WALKWAY_HPP_
 
 #include "rclcpp/rclcpp.hpp"
 
