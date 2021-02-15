e109cc9b18cf41b47b3d4f5de2187e440cf22896
Shinnosuke Hirakawa <shinnosuke.hirakawa@tier4.jp>
2021-02-15 22:42:26 +0900

    ros2 update

--- perception/util/saver/roi_image_saver/include/roi_image_saver/node.hpp
+++ perception/util/saver/roi_image_saver/include/roi_image_saver/node.hpp
@@ -11,23 +11,24 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#pragma once
-#include "ros/ros.h"
-
-#include "image_transport/image_transport.h"
-#include "image_transport/subscriber_filter.h"
-#include "autoware_perception_msgs/TrafficLightRoiArray.h"
-#include "message_filters/subscriber.h"
-#include "message_filters/sync_policies/approximate_time.h"
-#include "message_filters/synchronizer.h"
-#include "sensor_msgs/Image.h"
+#ifndef ROI_IMAGE_SAVER__NODE_HPP_
+#define ROI_IMAGE_SAVER__NODE_HPP_
 
 #include <memory>
 #include <string>
 
+#include "rclcpp/rclcpp.hpp"
+#include "autoware_perception_msgs/msg/traffic_light_roi_array.hpp"
+#include "image_transport/image_transport.hpp"
+#include "image_transport/subscriber_filter.hpp"
+#include "message_filters/subscriber.h"
+#include "message_filters/sync_policies/approximate_time.h"
+#include "message_filters/synchronizer.h"
+#include "sensor_msgs/msg/image.hpp"
+
 namespace traffic_light
 {
-class TrafficLightRoiImageSaver
+class TrafficLightRoiImageSaver : public rclcpp::Node
 {
 public:
   TrafficLightRoiImageSaver();
@@ -35,21 +36,21 @@ public:
 
 private:
   void imageRoiCallback(
-    const sensor_msgs::ImageConstPtr & input_image_msg,
-    const autoware_perception_msgs::TrafficLightRoiArray::ConstPtr & input_tl_roi_msg);
+    const sensor_msgs::msg::Image::ConstSharedPtr & input_image_msg,
+    const autoware_perception_msgs::msg::TrafficLightRoiArray::ConstSharedPtr & input_tl_roi_msg);
 
-  ros::NodeHandle nh_, pnh_;
-  image_transport::ImageTransport image_transport_;
   image_transport::SubscriberFilter image_sub_;
-  message_filters::Subscriber<autoware_perception_msgs::TrafficLightRoiArray> roi_sub_;
-  image_transport::Publisher image_pub_;
+  message_filters::Subscriber<autoware_perception_msgs::msg::TrafficLightRoiArray> roi_sub_;
+
   std::string save_dir_;
-  std::shared_ptr<ros::Rate> save_rate_ptr_;
+  std::shared_ptr<rclcpp::Rate> save_rate_ptr_;
   typedef message_filters::sync_policies::ApproximateTime<
-      sensor_msgs::Image, autoware_perception_msgs::TrafficLightRoiArray>
+      sensor_msgs::msg::Image, autoware_perception_msgs::msg::TrafficLightRoiArray>
     SyncPolicy;
   typedef message_filters::Synchronizer<SyncPolicy> Sync;
   Sync sync_;
 };
 
 }  // namespace traffic_light
+
+#endif  // ROI_IMAGE_SAVER__NODE_HPP_
