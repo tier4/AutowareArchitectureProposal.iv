6bea24dab9fe37c6c5569cb8cbdf752e0f3e68d2
Shigeki Kobayashi <shigeki.kobayashi@tier4.jp>
2021-02-22 19:54:26 +0900

    add test self pose listener

--- common/util/autoware_debug_tools/scripts/self_pose_listener.py
+++ common/util/autoware_debug_tools/scripts/self_pose_listener.py
@@ -20,35 +20,49 @@ from tf2_ros import LookupException
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 from geometry_msgs.msg import PoseStamped
-
+from rclpy.duration import Duration
 
 class SelfPoseListener(Node):
     def __init__(self):
         super().__init__('self_pose_listener')
         self.tf_buffer = Buffer()
         self._tf_listener = TransformListener(self.tf_buffer, self)
+        self._pub_pose = self.create_publisher(PoseStamped, "test_pose", 1)
+        self.timer = self.create_timer((1.0 / 10), self.get_current_pose)
 
     def get_current_pose(self):
         try:
-            (trans, quat) = self.tf_buffer.lookup_transform("map", "base_link", rclpy.time.Time())
-            time = self._tf_listener.getLatestCommonTime("map", "base_link")
-            return SelfPoseListener.create_pose(time, "map", trans, quat)
-        except LookupException:
+            tf = self.tf_buffer.lookup_transform("map", "base_link", rclpy.time.Time())
+            time = self.tf_buffer.get_latest_common_time("map", "base_link")
+            pose = SelfPoseListener.create_pose(time, "map", tf)
+            self._pub_pose.publish(pose)
+        except LookupException as e:
+            print(e)
             return None
 
     @staticmethod
-    def create_pose(time, frame_id, trans, quat):
+    def create_pose(time, frame_id, tf):
         pose = PoseStamped()
 
-        pose.header.stamp = time
+        pose.header.stamp = time.to_msg()
         pose.header.frame_id = frame_id
 
-        pose.pose.position.x = trans[0]
-        pose.pose.position.y = trans[1]
-        pose.pose.position.z = trans[2]
-        pose.pose.orientation.x = quat[0]
-        pose.pose.orientation.y = quat[1]
-        pose.pose.orientation.z = quat[2]
-        pose.pose.orientation.w = quat[3]
+        pose.pose.position.x = tf.transform.translation.x
+        pose.pose.position.y = tf.transform.translation.y
+        pose.pose.position.z = tf.transform.translation.z
+        pose.pose.orientation.x = tf.transform.rotation.x
+        pose.pose.orientation.y = tf.transform.rotation.y
+        pose.pose.orientation.z = tf.transform.rotation.z
+        pose.pose.orientation.w = tf.transform.rotation.w
 
         return pose
+
+def main():
+        rclpy.init()
+        self_pose_listener = SelfPoseListener()
+        rclpy.spin(self_pose_listener)
+        self_pose_listener.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
