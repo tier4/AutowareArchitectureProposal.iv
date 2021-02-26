7f19cd18ee90c00c2c7b4baa8c29f4a58bdcd39c
mitsudome-r <ryohsuke.mitsudome@tier4.jp>
2021-02-26 19:40:44 +0900

    fix test failures
    Signed-off-by: mitsudome-r <ryohsuke.mitsudome@tier4.jp>

--- sensing/preprocessor/pointcloud/livox_tag_filter/include/livox_tag_filter/livox_tag_filter_node.hpp
+++ sensing/preprocessor/pointcloud/livox_tag_filter/include/livox_tag_filter/livox_tag_filter_node.hpp
@@ -1,27 +1,26 @@
-/*
- * Copyright 2020 Tier IV, Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 Tier IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-#pragma once
-
-#include <string>
-#include <vector>
+#ifndef LIVOX_TAG_FILTER__LIVOX_TAG_FILTER_NODE_HPP_
+#define LIVOX_TAG_FILTER__LIVOX_TAG_FILTER_NODE_HPP_
 
 #include <rclcpp/rclcpp.hpp>
 
 #include <sensor_msgs/msg/point_cloud2.hpp>
+
+#include <memory>
+#include <vector>
 
 namespace livox_tag_filter
 {
@@ -31,7 +30,6 @@ public:
   LivoxTagFilterNode();
 
 private:
-
   // Parameter
   std::vector<std::int64_t> ignore_tags_;
 
@@ -44,3 +42,5 @@ private:
   rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub_pointcloud_;
 };
 }  // namespace livox_tag_filter
+
+#endif  // LIVOX_TAG_FILTER__LIVOX_TAG_FILTER_NODE_HPP_
--- sensing/preprocessor/pointcloud/livox_tag_filter/src/livox_tag_filter_node/livox_tag_filter_node.cpp
+++ sensing/preprocessor/pointcloud/livox_tag_filter/src/livox_tag_filter_node/livox_tag_filter_node.cpp
@@ -1,22 +1,24 @@
-/*
- * Copyright 2020 Tier IV, Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 Tier IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 
 #include "livox_tag_filter/livox_tag_filter_node.hpp"
 
 #include <pcl_conversions/pcl_conversions.h>
+
+#include <memory>
+#include <vector>
 
 struct LivoxPoint
 {
@@ -30,18 +32,22 @@ struct LivoxPoint
 
 POINT_CLOUD_REGISTER_POINT_STRUCT(
   LivoxPoint, (float, x, x)(float, y, y)(float, z, z)(float, intensity, intensity)(
-                std::uint8_t, tag, tag)(std::uint8_t, line, line))
+    std::uint8_t, tag, tag)(std::uint8_t, line, line))
 
 namespace livox_tag_filter
 {
-LivoxTagFilterNode::LivoxTagFilterNode() : Node("livox_tag_filter")
+LivoxTagFilterNode::LivoxTagFilterNode()
+: Node("livox_tag_filter")
 {
   // Parameter
   ignore_tags_ = this->declare_parameter("ignore_tags", std::vector<std::int64_t>{});
 
   // Subscriber
   using std::placeholders::_1;
-  sub_pointcloud_ = this->create_subscription<sensor_msgs::msg::PointCloud2>("input", 1, std::bind(&LivoxTagFilterNode::onPointCloud, this, _1));
+  sub_pointcloud_ =
+    this->create_subscription<sensor_msgs::msg::PointCloud2>(
+    "input", 1,
+    std::bind(&LivoxTagFilterNode::onPointCloud, this, _1));
 
   // Publisher
   pub_pointcloud_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("output", 1);
@@ -53,14 +59,14 @@ void LivoxTagFilterNode::onPointCloud(const sensor_msgs::msg::PointCloud2::Const
   pcl::fromROSMsg(*msg, points);
 
   const auto isIgnored = [&](const LivoxPoint & p) {
-    for (const auto & ignore_tag : ignore_tags_) {
-      if (p.tag == ignore_tag) {
-        return true;
+      for (const auto & ignore_tag : ignore_tags_) {
+        if (p.tag == ignore_tag) {
+          return true;
+        }
       }
-    }
 
-    return false;
-  };
+      return false;
+    };
 
   pcl::PointCloud<LivoxPoint> tag_filtered_points;
   for (const auto & p : points) {
