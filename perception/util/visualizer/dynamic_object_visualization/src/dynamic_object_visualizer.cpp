31dd7fd7d64a23f3b4a673ae7b5fc81464bd75e1
mitsudome-r <ryohsuke.mitsudome@tier4.jp>
2021-03-01 10:45:04 +0900

    fix include brackets
    Signed-off-by: mitsudome-r <ryohsuke.mitsudome@tier4.jp>

--- perception/util/visualizer/dynamic_object_visualization/src/dynamic_object_visualizer.cpp
+++ perception/util/visualizer/dynamic_object_visualization/src/dynamic_object_visualizer.cpp
@@ -12,13 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#define EIGEN_MPL2_ONLY
-#include <Eigen/Core>
-#include <Eigen/Eigen>
-
 #include <memory>
 #include <string>
 #include <vector>
+
+#define EIGEN_MPL2_ONLY
+#include "Eigen/Core"
+#include "Eigen/Eigen"
 
 #include "dynamic_object_visualization/dynamic_object_visualizer.hpp"
 #include "geometry_msgs/msg/point.hpp"
