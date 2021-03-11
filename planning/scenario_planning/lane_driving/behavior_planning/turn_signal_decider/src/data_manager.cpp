ac1fa3f229bad7d8b2b8b057905e20c1feabd6e1
Kenji Miyake <kenji.miyake@tier4.jp>
2021-03-11 12:40:02 +0900

    Fix test
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- planning/scenario_planning/lane_driving/behavior_planning/turn_signal_decider/src/data_manager.cpp
+++ planning/scenario_planning/lane_driving/behavior_planning/turn_signal_decider/src/data_manager.cpp
@@ -11,7 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 #include <memory>
+#include <string>
 
 #include "lanelet2_extension/utility/message_conversion.hpp"
 #include "lanelet2_extension/utility/utilities.hpp"
