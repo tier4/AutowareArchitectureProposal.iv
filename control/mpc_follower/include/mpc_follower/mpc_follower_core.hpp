bee3b622b35c74581e183b35af19d5829bf94a0e
Kazuki Miyahara <kmiya@outlook.com>
2021-04-07 13:09:36 +0900

    Fix an identical code for different branches (#1231)

--- control/mpc_follower/include/mpc_follower/mpc_follower_core.hpp
+++ control/mpc_follower/include/mpc_follower/mpc_follower_core.hpp
@@ -388,7 +388,7 @@ private:
   double getWeightSteerInputSqVel(const double curvature)
   {
     return isLowCurvature(curvature) ? mpc_param_.low_curvature_weight_steering_input_squared_vel :
-           mpc_param_.low_curvature_weight_steering_input_squared_vel;
+           mpc_param_.weight_steering_input_squared_vel;
   }
 
   double getWeightLatJerk(const double curvature)
