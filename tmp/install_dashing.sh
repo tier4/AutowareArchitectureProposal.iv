e20c58034d1448a5534a4507f44c37ba0dddbf4a
Daisuke Nishimatsu <42202095+wep21@users.noreply.github.com>
2021-06-15 00:33:00 +0900

    Remove tmp directory (#1456)
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>

--- tmp/install_dashing.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo apt update && sudo apt install curl gnupg2 lsb-release -y
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
-sudo apt update
-sudo apt install ros-dashing-desktop -y
