5ca77b441faea6f9b5c759e050228c2c2490af08
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2021-07-16 18:35:16 +0200

    [#1221] Add co-developed entry to copyright

--- common/util/rviz_plugins/autoware_rviz_plugins/src/common/color_alpha_property.cpp
+++ common/util/rviz_plugins/autoware_rviz_plugins/src/common/color_alpha_property.cpp
@@ -11,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// Co-developed by Tier IV, Inc. and Apex.AI, Inc.
 
 #include <common/color_alpha_property.hpp>
 
