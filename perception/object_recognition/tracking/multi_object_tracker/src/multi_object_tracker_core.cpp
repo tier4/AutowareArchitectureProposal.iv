9cb97c97823f5929bbc26df713ddb2ac4fba3524
Daisuke Nishimatsu <42202095+wep21@users.noreply.github.com>
2021-07-28 19:45:42 +0900

    Feature/porting modify tracker life cycle (#1675)
    * Modify tracker life cycle (#1462)
    
    * add z lpf
    
    Signed-off-by: Yukihiro Saito <yukky.saito@gmail.com>
    
    * modify life cycle
    
    Signed-off-by: Yukihiro Saito <yukky.saito@gmail.com>
    
    * refactor
    
    * apply format
    
    * fix typo
    
    * add read me
    
    * fix typo
    
    * change readme
    
    * modify readme
    
    * fix typo
    
    * cosmetic change
    
    * cosmetic change
    
    * refactor and change removal conditions
    
    * cosmetic change
    
    * add note
    
    * bug fix
    
    * change specific alive pattern
    
    * format code
    
    Co-authored-by: Takamasa Horibe <horibe.takamasa@gmail.com>
    
    * Apply lint
    
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>
    
    * pre-commit fixes
    
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>
    
    * fix bug
    
    Co-authored-by: Yukihiro Saito <yukky.saito@gmail.com>
    Co-authored-by: Takamasa Horibe <horibe.takamasa@gmail.com>
    Co-authored-by: tkimura4 <tomoya.kimura@tier4.jp>

--- perception/object_recognition/tracking/multi_object_tracker/src/multi_object_tracker_core.cpp
+++ perception/object_recognition/tracking/multi_object_tracker/src/multi_object_tracker_core.cpp
@@ -22,6 +22,8 @@
 #include <utility>
 #include <vector>
 
+#include "boost/optional.hpp"
+
 #include "tf2_ros/create_timer_ros.h"
 #include "tf2_ros/create_timer_interface.h"
 
@@ -35,6 +37,124 @@
 
 using SemanticType = autoware_perception_msgs::msg::Semantic;
 
+namespace
+{
+boost::optional<geometry_msgs::msg::Transform> getTransform(
+  const tf2_ros::Buffer & tf_buffer, const std::string & source_frame_id,
+  const std::string & target_frame_id, const rclcpp::Time & time)
+{
+  try {
+    geometry_msgs::msg::TransformStamped self_transform_stamped;
+    self_transform_stamped = tf_buffer.lookupTransform(
+      /*target*/ target_frame_id, /*src*/ source_frame_id, time,
+      rclcpp::Duration::from_seconds(0.5));
+    return self_transform_stamped.transform;
+  } catch (tf2::TransformException & ex) {
+    RCLCPP_WARN_STREAM(rclcpp::get_logger("multi_object_tracker"), ex.what());
+    return boost::none;
+  }
+}
+
+bool transformDynamicObjects(
+  const autoware_perception_msgs::msg::DynamicObjectWithFeatureArray & input_msg,
+  const std::string & target_frame_id, const tf2_ros::Buffer & tf_buffer,
+  autoware_perception_msgs::msg::DynamicObjectWithFeatureArray & output_msg)
+{
+  output_msg = input_msg;
+
+  /* transform to world coordinate */
+  if (input_msg.header.frame_id != target_frame_id) {
+    tf2::Transform tf_target2objects_world;
+    tf2::Transform tf_target2objects;
+    tf2::Transform tf_objects_world2objects;
+    {
+      const auto ros_target2objects_world =
+        getTransform(tf_buffer, input_msg.header.frame_id, target_frame_id, input_msg.header.stamp);
+      if (!ros_target2objects_world) {return false;}
+      tf2::fromMsg(*ros_target2objects_world, tf_target2objects_world);
+    }
+    for (size_t i = 0; i < output_msg.feature_objects.size(); ++i) {
+      tf2::fromMsg(
+        output_msg.feature_objects.at(i).object.state.pose_covariance.pose,
+        tf_objects_world2objects);
+      tf_target2objects = tf_target2objects_world * tf_objects_world2objects;
+      tf2::toMsg(
+        tf_target2objects, output_msg.feature_objects.at(i).object.state.pose_covariance.pose);
+    }
+  }
+  return true;
+}
+
+inline float getVelocity(const autoware_perception_msgs::msg::DynamicObject & object)
+{
+  return std::hypot(
+    object.state.twist_covariance.twist.linear.x, object.state.twist_covariance.twist.linear.y);
+}
+
+inline geometry_msgs::msg::Pose getPose(
+  const autoware_perception_msgs::msg::DynamicObject & object)
+{
+  return object.state.pose_covariance.pose;
+}
+
+float getXYSquareDistance(
+  const geometry_msgs::msg::Transform & self_transform,
+  const autoware_perception_msgs::msg::DynamicObject & object)
+{
+  const auto object_pos = getPose(object).position;
+  const float x = self_transform.translation.x - object_pos.x;
+  const float y = self_transform.translation.y - object_pos.y;
+  return x * x + y * y;
+}
+
+/**
+ * @brief If the tracker is stable at a low speed and has a vehicle type, it will keep
+ * tracking for a longer time to deal with detection lost due to occlusion, etc.
+ * @param tracker The tracker to be determined.
+ * @param time Target time to determine.
+ * @param self_transform Position of the vehicle at the target time.
+ * @return Result of deciding whether to leave tracker or not.
+ */
+bool isSpecificAlivePattern(
+  const std::shared_ptr<const Tracker> & tracker, const rclcpp::Time & time,
+  const geometry_msgs::msg::Transform & self_transform)
+{
+  autoware_perception_msgs::msg::DynamicObject object;
+  tracker->getEstimatedDynamicObject(time, object);
+
+  constexpr float min_detection_rate = 0.2;
+  constexpr int min_measurement_count = 5;
+  constexpr float max_elapsed_time = 10.0;
+  constexpr float max_velocity = 1.0;
+  constexpr float max_distance = 100.0;
+
+  const float detection_rate =
+    tracker->getTotalMeasurementCount() /
+    (tracker->getTotalNoMeasurementCount() + tracker->getTotalMeasurementCount());
+
+  const bool big_vehicle = tracker->getType() == SemanticType::TRUCK ||
+    tracker->getType() == SemanticType::BUS;
+
+  const bool slow_velocity = getVelocity(object) < max_velocity;
+
+  const bool high_confidence =
+    (min_detection_rate < detection_rate ||
+    min_measurement_count < tracker->getTotalMeasurementCount());
+
+  const bool not_too_far =
+    getXYSquareDistance(self_transform, object) < max_distance * max_distance;
+
+  const bool within_max_survival_period = tracker->getElapsedTimeFromLastUpdate(time) <
+    max_elapsed_time;
+
+  const bool is_specific_alive_pattern =
+    high_confidence && big_vehicle && within_max_survival_period && not_too_far && slow_velocity;
+
+  return is_specific_alive_pattern;
+}
+
+}  // namespace
+
 MultiObjectTracker::MultiObjectTracker(const rclcpp::NodeOptions & node_options)
 : rclcpp::Node("multi_object_tracker", node_options),
   tf_buffer_(this->get_clock()),
@@ -46,28 +166,30 @@ MultiObjectTracker::MultiObjectTracker(const rclcpp::NodeOptions & node_options)
   dynamic_object_sub_ =
     create_subscription<autoware_perception_msgs::msg::DynamicObjectWithFeatureArray>(
     "input", durable_qos,
-    std::bind(&MultiObjectTracker::measurementCallback, this, std::placeholders::_1));
+    std::bind(&MultiObjectTracker::onMeasurement, this, std::placeholders::_1));
   dynamic_object_pub_ =
     create_publisher<autoware_perception_msgs::msg::DynamicObjectArray>("output", rclcpp::QoS{1});
 
   // Parameters
   double publish_rate = declare_parameter<double>("publish_rate", 30.0);
   world_frame_id_ = declare_parameter<std::string>("world_frame_id", "world");
-  enable_delay_compensation_ = declare_parameter<bool>("enable_delay_compensation", false);
+  bool enable_delay_compensation{declare_parameter("enable_delay_compensation", false)};
 
   auto cti = std::make_shared<tf2_ros::CreateTimerROS>(
     this->get_node_base_interface(), this->get_node_timers_interface());
   tf_buffer_.setCreateTimerInterface(cti);
 
   // Create ROS time based timer
-  auto timer_callback = std::bind(&MultiObjectTracker::publishTimerCallback, this);
-  auto period = std::chrono::duration_cast<std::chrono::nanoseconds>(
-    std::chrono::duration<double>(1.0 / publish_rate));
+  if (enable_delay_compensation) {
+    auto timer_callback = std::bind(&MultiObjectTracker::onTimer, this);
+    auto period = std::chrono::duration_cast<std::chrono::nanoseconds>(
+      std::chrono::duration<double>(1.0 / publish_rate));
 
-  publish_timer_ = std::make_shared<rclcpp::GenericTimer<decltype(timer_callback)>>(
-    this->get_clock(), period, std::move(timer_callback),
-    this->get_node_base_interface()->get_context());
-  this->get_node_timers_interface()->add_timer(publish_timer_, nullptr);
+    publish_timer_ = std::make_shared<rclcpp::GenericTimer<decltype(timer_callback)>>(
+      this->get_clock(), period, std::move(timer_callback),
+      this->get_node_base_interface()->get_context());
+    this->get_node_timers_interface()->add_timer(publish_timer_, nullptr);
+  }
 
   this->declare_parameter("can_assign_matrix");
   this->declare_parameter("max_dist_matrix");
@@ -89,14 +211,18 @@ MultiObjectTracker::MultiObjectTracker(const rclcpp::NodeOptions & node_options)
     can_assign_matrix, max_dist_matrix, max_area_matrix, min_area_matrix, max_rad_matrix);
 }
 
-void MultiObjectTracker::measurementCallback(
+void MultiObjectTracker::onMeasurement(
   const autoware_perception_msgs::msg::DynamicObjectWithFeatureArray::ConstSharedPtr
   input_objects_msg)
 {
+  const auto self_transform =
+    getTransform(tf_buffer_, "base_link", world_frame_id_, input_objects_msg->header.stamp);
+  if (!self_transform) {return;}
+
   /* transform to world coordinate */
-  autoware_perception_msgs::msg::DynamicObjectWithFeatureArray input_transformed_objects;
-  if (!MultiObjectTracker::transformDynamicObjects(
-      *input_objects_msg, world_frame_id_, input_transformed_objects))
+  autoware_perception_msgs::msg::DynamicObjectWithFeatureArray transformed_objects;
+  if (!transformDynamicObjects(
+      *input_objects_msg, world_frame_id_, tf_buffer_, transformed_objects))
   {
     return;
   }
@@ -109,7 +235,7 @@ void MultiObjectTracker::measurementCallback(
   /* global nearest neighbor */
   std::unordered_map<int, int> direct_assignment, reverse_assignment;
   Eigen::MatrixXd score_matrix = data_association_->calcScoreMatrix(
-    input_transformed_objects, list_tracker_);  // row : tracker, col : measurement
+    transformed_objects, list_tracker_);  // row : tracker, col : measurement
   data_association_->assign(score_matrix, direct_assignment, reverse_assignment);
 
   /* tracker measurement update */
@@ -120,7 +246,7 @@ void MultiObjectTracker::measurementCallback(
     if (direct_assignment.find(tracker_idx) != direct_assignment.end()) {  // found
       (*(tracker_itr))
       ->updateWithMeasurement(
-        input_transformed_objects.feature_objects.at(direct_assignment.find(tracker_idx)->second)
+        transformed_objects.feature_objects.at(direct_assignment.find(tracker_idx)->second)
         .object,
         measurement_time);
     } else {  // not found
@@ -129,97 +255,78 @@ void MultiObjectTracker::measurementCallback(
   }
 
   /* life cycle check */
-  checkTrackerLifeCycle(list_tracker_, measurement_time);
+  checkTrackerLifeCycle(list_tracker_, measurement_time, *self_transform);
+  /* sanitize trackers */
+  sanitizeTracker(list_tracker_, measurement_time);
 
   /* new tracker */
-  for (size_t i = 0; i < input_transformed_objects.feature_objects.size(); ++i) {
+  for (size_t i = 0; i < transformed_objects.feature_objects.size(); ++i) {
     if (reverse_assignment.find(i) != reverse_assignment.end()) {  // found
       continue;
     }
-    const int & type = input_transformed_objects.feature_objects.at(i).object.semantic.type;
-    if (type == SemanticType::CAR || type == SemanticType::TRUCK || type == SemanticType::BUS) {
-      list_tracker_.push_back(
-        std::make_shared<MultipleVehicleTracker>(
-          measurement_time, input_transformed_objects.feature_objects.at(i).object));
-    } else if (type == SemanticType::PEDESTRIAN) {
-      list_tracker_.push_back(
-        std::make_shared<PedestrianAndBicycleTracker>(
-          measurement_time, input_transformed_objects.feature_objects.at(i).object));
-    } else if (type == SemanticType::BICYCLE || type == SemanticType::MOTORBIKE) {
-      list_tracker_.push_back(
-        std::make_shared<PedestrianAndBicycleTracker>(
-          measurement_time, input_transformed_objects.feature_objects.at(i).object));
-    } else {
-      list_tracker_.push_back(
-        std::make_shared<UnknownTracker>(
-          measurement_time, input_transformed_objects.feature_objects.at(i).object));
-    }
+    list_tracker_.push_back(
+      createNewTracker(transformed_objects.feature_objects.at(i).object, measurement_time));
   }
 
-  if (!enable_delay_compensation_) {
+  if (publish_timer_ == nullptr) {
     publish(measurement_time);
   }
 }
 
-void MultiObjectTracker::publishTimerCallback()
+std::shared_ptr<Tracker> MultiObjectTracker::createNewTracker(
+  const autoware_perception_msgs::msg::DynamicObject & object, const rclcpp::Time & time) const
 {
-  if (enable_delay_compensation_) {
-    rclcpp::Time current_time = this->now();
-    /* life cycle check */
-    checkTrackerLifeCycle(list_tracker_, current_time);
-
-    // Publish
-    publish(current_time);
-    return;
+  const int & type = object.semantic.type;
+  if (type == SemanticType::CAR || type == SemanticType::TRUCK || type == SemanticType::BUS) {
+    return std::make_shared<MultipleVehicleTracker>(time, object);
+  } else if (type == SemanticType::PEDESTRIAN) {
+    return std::make_shared<PedestrianAndBicycleTracker>(time, object);
+  } else if (type == SemanticType::BICYCLE || type == SemanticType::MOTORBIKE) {
+    return std::make_shared<PedestrianAndBicycleTracker>(time, object);
+  } else {
+    return std::make_shared<UnknownTracker>(time, object);
   }
 }
 
-bool MultiObjectTracker::transformDynamicObjects(
-  const autoware_perception_msgs::msg::DynamicObjectWithFeatureArray & input_msg,
-  const std::string & target_frame_id,
-  autoware_perception_msgs::msg::DynamicObjectWithFeatureArray & output_msg)
+void MultiObjectTracker::onTimer()
 {
-  output_msg = input_msg;
+  rclcpp::Time current_time = this->now();
+  const auto self_transform = getTransform(tf_buffer_, world_frame_id_, "base_link", current_time);
+  if (!self_transform) {return;}
 
-  /* transform to world coordinate */
-  if (input_msg.header.frame_id != target_frame_id) {
-    tf2::Transform tf_target2objects_world;
-    tf2::Transform tf_target2objects;
-    tf2::Transform tf_objects_world2objects;
-    try {
-      geometry_msgs::msg::TransformStamped ros_target2objects_world;
-      ros_target2objects_world = tf_buffer_.lookupTransform(
-        /*target*/ target_frame_id, /*src*/ input_msg.header.frame_id, input_msg.header.stamp,
-        rclcpp::Duration::from_seconds(0.5));
-      tf2::fromMsg(ros_target2objects_world.transform, tf_target2objects_world);
-    } catch (tf2::TransformException & ex) {
-      RCLCPP_WARN(this->get_logger(), "%s", ex.what());
-      return false;
-    }
-    for (size_t i = 0; i < output_msg.feature_objects.size(); ++i) {
-      tf2::fromMsg(
-        output_msg.feature_objects.at(i).object.state.pose_covariance.pose,
-        tf_objects_world2objects);
-      tf_target2objects = tf_target2objects_world * tf_objects_world2objects;
-      tf2::toMsg(
-        tf_target2objects, output_msg.feature_objects.at(i).object.state.pose_covariance.pose);
-    }
-  }
-  return true;
+  /* life cycle check */
+  checkTrackerLifeCycle(list_tracker_, current_time, *self_transform);
+  /* sanitize trackers */
+  sanitizeTracker(list_tracker_, current_time);
+
+  // Publish
+  publish(current_time);
 }
 
 void MultiObjectTracker::checkTrackerLifeCycle(
-  std::list<std::shared_ptr<Tracker>> & list_tracker, const rclcpp::Time & time)
+  std::list<std::shared_ptr<Tracker>> & list_tracker, const rclcpp::Time & time,
+  const geometry_msgs::msg::Transform & self_transform)
 {
-  /* delete old tracker */
+  /* params */
+  constexpr float max_elapsed_time = 1.0;
+
+  /* delete tracker */
   for (auto itr = list_tracker.begin(); itr != list_tracker.end(); ++itr) {
-    if (1.0 < (*itr)->getElapsedTimeFromLastUpdate(this->now())) {
+    const bool is_old = max_elapsed_time < (*itr)->getElapsedTimeFromLastUpdate(time);
+    const bool is_specific_alive_pattern = isSpecificAlivePattern(*itr, time, self_transform);
+    if (is_old && !is_specific_alive_pattern) {
       auto erase_itr = itr;
       --itr;
       list_tracker.erase(erase_itr);
     }
   }
+}
 
+void MultiObjectTracker::sanitizeTracker(
+  std::list<std::shared_ptr<Tracker>> & list_tracker, const rclcpp::Time & time)
+{
+  constexpr float min_iou = 0.1;
+  constexpr double distance_threshold = 5.0;
   /* delete collision tracker */
   for (auto itr1 = list_tracker.begin(); itr1 != list_tracker.end(); ++itr1) {
     autoware_perception_msgs::msg::DynamicObject object1;
@@ -227,14 +334,13 @@ void MultiObjectTracker::checkTrackerLifeCycle(
     for (auto itr2 = std::next(itr1); itr2 != list_tracker.end(); ++itr2) {
       autoware_perception_msgs::msg::DynamicObject object2;
       (*itr2)->getEstimatedDynamicObject(time, object2);
-      constexpr double distance_threshold = 5.0f;
       const double distance = std::hypot(
         object1.state.pose_covariance.pose.position.x -
         object2.state.pose_covariance.pose.position.x,
         object1.state.pose_covariance.pose.position.y -
         object2.state.pose_covariance.pose.position.y);
       if (distance_threshold < distance) {continue;}
-      if (0.1 < utils::get2dIoU(object1, object2)) {
+      if (min_iou < utils::get2dIoU(object1, object2)) {
         if ((*itr1)->getTotalMeasurementCount() < (*itr2)->getTotalMeasurementCount()) {
           itr1 = list_tracker.erase(itr1);
           --itr1;
@@ -248,7 +354,15 @@ void MultiObjectTracker::checkTrackerLifeCycle(
   }
 }
 
-void MultiObjectTracker::publish(const rclcpp::Time & time)
+inline bool MultiObjectTracker::shouldTrackerPublish(
+  const std::shared_ptr<const Tracker> tracker) const
+{
+  constexpr int measurement_count_threshold = 3;
+  if (tracker->getTotalMeasurementCount() < measurement_count_threshold) {return false;}
+  return true;
+}
+
+void MultiObjectTracker::publish(const rclcpp::Time & time) const
 {
   const auto subscriber_count = dynamic_object_pub_->get_subscription_count() +
     dynamic_object_pub_->get_intra_process_subscription_count();
@@ -258,7 +372,7 @@ void MultiObjectTracker::publish(const rclcpp::Time & time)
   output_msg.header.frame_id = world_frame_id_;
   output_msg.header.stamp = time;
   for (auto itr = list_tracker_.begin(); itr != list_tracker_.end(); ++itr) {
-    if ((*itr)->getTotalMeasurementCount() < 3) {continue;}
+    if (!shouldTrackerPublish(*itr)) {continue;}
     autoware_perception_msgs::msg::DynamicObject object;
     (*itr)->getEstimatedDynamicObject(time, object);
     output_msg.objects.push_back(object);
