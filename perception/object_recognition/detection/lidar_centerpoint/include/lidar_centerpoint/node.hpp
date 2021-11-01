9b95d3c2fd5cd123406a51fac1357557855ad6fd
Yusuke Muramatsu <yukke42@users.noreply.github.com>
2021-11-01 21:38:47 +0900

    update CenterPoint  (#2222)
    * update to model trained by mmdet3d
    
    * add vizualizer (debug)
    
    * for multi-frame inputs
    
    * chagne config
    
    * use autoware_utils::pi
    
    * project specific model and param
    
    * rename vfe -> encoder
    
    * rename general to common
    
    * update download link
    
    * update
    
    * fix
    
    * rename model_name
    
    * change training toolbox link
    
    * chage lint package
    
    * fix test error
    
    * commit suggestion

--- perception/object_recognition/detection/lidar_centerpoint/include/lidar_centerpoint/node.hpp
+++ perception/object_recognition/detection/lidar_centerpoint/include/lidar_centerpoint/node.hpp
@@ -25,6 +25,7 @@
 
 #include <memory>
 #include <string>
+#include <vector>
 
 namespace centerpoint
 {
@@ -36,6 +37,8 @@ public:
 private:
   void pointCloudCallback(const sensor_msgs::msg::PointCloud2::ConstSharedPtr input_pointcloud_msg);
 
+  static uint8_t getSemanticType(const std::string & class_name);
+
   rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr pointcloud_sub_;
   rclcpp::Publisher<autoware_perception_msgs::msg::DynamicObjectWithFeatureArray>::SharedPtr
     objects_pub_;
@@ -44,16 +47,19 @@ private:
   float score_threshold_{0.0};
   std::string densification_base_frame_;
   int densification_past_frames_{0};
-  bool use_vfe_trt_{false};
+  bool use_encoder_trt_{false};
   bool use_head_trt_{false};
   std::string trt_precision_;
 
-  std::string vfe_onnx_path_;
-  std::string vfe_engine_path_;
-  std::string vfe_pt_path_;
+  std::string encoder_onnx_path_;
+  std::string encoder_engine_path_;
+  std::string encoder_pt_path_;
   std::string head_onnx_path_;
   std::string head_engine_path_;
   std::string head_pt_path_;
+
+  std::vector<std::string> class_names_;
+  bool rename_car_to_truck_and_bus_{false};
 
   std::unique_ptr<PointCloudDensification> densification_ptr_{nullptr};
   std::unique_ptr<CenterPointTRT> detector_ptr_{nullptr};
