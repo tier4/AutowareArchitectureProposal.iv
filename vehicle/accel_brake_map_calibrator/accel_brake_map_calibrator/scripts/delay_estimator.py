6e661edf0a7c80e19ac6d95b33ae6b59b3330a4f
Kenji Miyake <kenji.miyake@tier4.jp>
2021-11-03 12:25:54 +0900

    Apply Black
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- vehicle/accel_brake_map_calibrator/accel_brake_map_calibrator/scripts/delay_estimator.py
+++ vehicle/accel_brake_map_calibrator/accel_brake_map_calibrator/scripts/delay_estimator.py
@@ -26,45 +26,45 @@ from rclpy.node import Node
 
 
 class DelayEstimator(Node):
-
     def __init__(self):
         # get parameter
-        super().__init__('delay_estimator')
-        package_path = get_package_share_directory('accel_brake_map_calibrator')
-        self.declare_parameter('log_file', package_path + '/config/log.csv')
-        log_file = self.get_parameter('log_file').get_parameter_value().string_value
-        self.declare_parameter('velocity_min_threshold', 0.1)
-        min_vel_thr = self.get_parameter(
-            'velocity_min_threshold').get_parameter_value().double_value
-        self.declare_parameter('velocity_diff_threshold', 0.556)
-        vel_diff_thr = self.get_parameter(
-            'velocity_diff_threshold').get_parameter_value().double_value
-        self.declare_parameter('pedal_diff_threshold', 0.03)
-        pedal_diff_thr = self.get_parameter(
-            'pedal_diff_threshold').get_parameter_value().double_value
-        self.declare_parameter('max_steer_threshold', 0.2)
-        max_steer_thr = self.get_parameter(
-            'max_steer_threshold').get_parameter_value().double_value
-        self.declare_parameter('max_pitch_threshold', 0.02)
-        max_pitch_thr = self.get_parameter(
-            'max_pitch_threshold').get_parameter_value().double_value
-        self.declare_parameter('max_jerk_threshold', 0.7)
-        max_jerk_thr = self.get_parameter(
-            'max_jerk_threshold').get_parameter_value().double_value
-        self.declare_parameter('pedal_velocity_thresh', 0.15)
-        max_pedal_vel_thr = self.get_parameter('pedal_velocity_thresh'
-                                               ).get_parameter_value().double_value
-        self.declare_parameter('update_hz', 10.0)
-        update_hz = self.get_parameter('update_hz').get_parameter_value().double_value
+        super().__init__("delay_estimator")
+        package_path = get_package_share_directory("accel_brake_map_calibrator")
+        self.declare_parameter("log_file", package_path + "/config/log.csv")
+        log_file = self.get_parameter("log_file").get_parameter_value().string_value
+        self.declare_parameter("velocity_min_threshold", 0.1)
+        min_vel_thr = (
+            self.get_parameter("velocity_min_threshold").get_parameter_value().double_value
+        )
+        self.declare_parameter("velocity_diff_threshold", 0.556)
+        vel_diff_thr = (
+            self.get_parameter("velocity_diff_threshold").get_parameter_value().double_value
+        )
+        self.declare_parameter("pedal_diff_threshold", 0.03)
+        pedal_diff_thr = (
+            self.get_parameter("pedal_diff_threshold").get_parameter_value().double_value
+        )
+        self.declare_parameter("max_steer_threshold", 0.2)
+        max_steer_thr = self.get_parameter("max_steer_threshold").get_parameter_value().double_value
+        self.declare_parameter("max_pitch_threshold", 0.02)
+        max_pitch_thr = self.get_parameter("max_pitch_threshold").get_parameter_value().double_value
+        self.declare_parameter("max_jerk_threshold", 0.7)
+        max_jerk_thr = self.get_parameter("max_jerk_threshold").get_parameter_value().double_value
+        self.declare_parameter("pedal_velocity_thresh", 0.15)
+        max_pedal_vel_thr = (
+            self.get_parameter("pedal_velocity_thresh").get_parameter_value().double_value
+        )
+        self.declare_parameter("update_hz", 10.0)
+        update_hz = self.get_parameter("update_hz").get_parameter_value().double_value
 
         self.data_span = 1.0 / update_hz
 
         # read csv
-        self.cr = CSVReader(log_file, csv_type='file')
+        self.cr = CSVReader(log_file, csv_type="file")
         # remove unused_data
         self.csv_data = self.cr.removeUnusedData(
-            min_vel_thr, max_steer_thr, max_pitch_thr,
-            max_pedal_vel_thr, max_jerk_thr)
+            min_vel_thr, max_steer_thr, max_pitch_thr, max_pedal_vel_thr, max_jerk_thr
+        )
 
         # get statistics array
         vel_data = self.cr.getVelData()
@@ -75,43 +75,42 @@ class DelayEstimator(Node):
         color_data = self.cr.getPitchData()
 
         data, full_data = CalcUtils.create_2d_map(
-            vel_data, pedal_data, acc_data, color_data,
-            CF.VEL_LIST/3.6, vel_diff_thr,
-            CF.PEDAL_LIST, pedal_diff_thr)
+            vel_data,
+            pedal_data,
+            acc_data,
+            color_data,
+            CF.VEL_LIST / 3.6,
+            vel_diff_thr,
+            CF.PEDAL_LIST,
+            pedal_diff_thr,
+        )
 
         # plot all data
         # value to use for statistics
         PEDAL_VALUE = 0.1
-        VEL_VALUE_LIST = np.array([10, 15, 20])/3.6
+        VEL_VALUE_LIST = np.array([10, 15, 20]) / 3.6
         plotter = Plotter(3, 2)
         max_delay_step = 5
         for delay_step in range(max_delay_step + 1):
-            print('data processing... ' + str(delay_step) + ' / ' + str(max_delay_step))
-            csv_data = \
-                self.cr.extractPedalRangeWithDelay(
-                    delay_step, PEDAL_VALUE,
-                    pedal_diff_thr)
+            print("data processing... " + str(delay_step) + " / " + str(max_delay_step))
+            csv_data = self.cr.extractPedalRangeWithDelay(delay_step, PEDAL_VALUE, pedal_diff_thr)
 
             # get correlation coefficient
             # extract data of velocity is VEL_VALUE
             coef_list = []
             for vel_value in VEL_VALUE_LIST:
-                ex_csv_data = csv_data[csv_data[CF.VEL]
-                                       < vel_value + vel_diff_thr]
-                ex_csv_data = csv_data[csv_data[CF.VEL]
-                                       > vel_value - vel_diff_thr]
-                pedal_speed = ex_csv_data[CF.A_PED_SPD] - \
-                    ex_csv_data[CF.B_PED_SPD]
+                ex_csv_data = csv_data[csv_data[CF.VEL] < vel_value + vel_diff_thr]
+                ex_csv_data = csv_data[csv_data[CF.VEL] > vel_value - vel_diff_thr]
+                pedal_speed = ex_csv_data[CF.A_PED_SPD] - ex_csv_data[CF.B_PED_SPD]
                 accel = ex_csv_data[CF.ACC]
                 coef = self.getCorCoef(pedal_speed, accel)
                 coef_list.append(coef)
 
-            print('delay_step: ', delay_step)
-            print('coef: ', coef_list)
+            print("delay_step: ", delay_step)
+            print("coef: ", coef_list)
 
             ave_coef = np.average(coef_list)
-            self.plotPedalSpeedAndAccel(
-                csv_data, plotter, delay_step + 1, delay_step, ave_coef)
+            self.plotPedalSpeedAndAccel(csv_data, plotter, delay_step + 1, delay_step, ave_coef)
         plotter.show()
 
     def getCorCoef(self, a, b):
@@ -123,15 +122,14 @@ class DelayEstimator(Node):
         accel = csv_data[CF.ACC]
         velocity = csv_data[CF.VEL] * 3.6
         fig = plotter.subplot(subplot_num)
-        plotter.scatter_color(pedal_speed, accel, velocity, 'hsv', label=None)
+        plotter.scatter_color(pedal_speed, accel, velocity, "hsv", label=None)
         delay_time_ms = delay_step * self.data_span * 1000
         plotter.add_label(
-            'pedal-spd-acc (delay = ' +
-            str(delay_time_ms) + ' ms), R = ' + str(coef),
-            'pedal-speed', 'accel')
-        plotter.set_lim(fig,
-                        [-0.4, 0.4],
-                        [-1.0, 1.0])
+            "pedal-spd-acc (delay = " + str(delay_time_ms) + " ms), R = " + str(coef),
+            "pedal-speed",
+            "accel",
+        )
+        plotter.set_lim(fig, [-0.4, 0.4], [-1.0, 1.0])
 
 
 def main(args=None):
@@ -142,5 +140,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- vehicle/accel_brake_map_calibrator/accel_brake_map_calibrator/scripts/plotter.py
+++ vehicle/accel_brake_map_calibrator/accel_brake_map_calibrator/scripts/plotter.py
@@ -20,7 +20,6 @@ import numpy as np
 
 
 class Plotter(object):
-
     def __init__(self, total_height, total_width, plot=True):
         self.total_height = total_height
         self.total_width = total_width
@@ -36,14 +35,13 @@ class Plotter(object):
     def subplot_more(self, plot_num):
         width = plot_num % self.total_width
         height = int((plot_num - width) / self.total_width)
-        fig = plt.subplot2grid(
-            (self.total_height, self.total_width), (height, width))
+        fig = plt.subplot2grid((self.total_height, self.total_width), (height, width))
         return fig
 
     def plot_1d(self, data, color, label):
         plt.plot(data, color, label=label)
 
-    def plot(self, x_data, y_data, color, label=None, linestyle='solid'):
+    def plot(self, x_data, y_data, color, label=None, linestyle="solid"):
         plt.plot(x_data, y_data, color, label=label, linestyle=linestyle, zorder=-1)
 
     def scatter(self, x, y, color, label=None, colorbar=False):
@@ -53,46 +51,57 @@ class Plotter(object):
         sc = plt.scatter(x, y, c=color, label=label, cmap=cmap, s=3)
         plt.colorbar(sc)
 
-    def plot_text(self, x, y, val, num_data_type='float', color='black'):
-        if num_data_type == 'float':
-            plt.text(x, y, '{0:.2f}'.format(
-                float(val)),
-                horizontalalignment='center',
-                verticalalignment='center',
-                color=color, clip_on=True,
-                fontsize=8)
-        elif num_data_type == 'int':
-            plt.text(x, y, '{0:d}'.format(
-                int(val)),
-                horizontalalignment='center',
-                verticalalignment='center',
-                color=color, clip_on=True,
-                fontsize=8)
-        elif num_data_type == 'str':
-            plt.text(x, y, val,
-                     horizontalalignment='center',
-                     verticalalignment='center',
-                     color=color, clip_on=True,
-                     fontsize=8)
+    def plot_text(self, x, y, val, num_data_type="float", color="black"):
+        if num_data_type == "float":
+            plt.text(
+                x,
+                y,
+                "{0:.2f}".format(float(val)),
+                horizontalalignment="center",
+                verticalalignment="center",
+                color=color,
+                clip_on=True,
+                fontsize=8,
+            )
+        elif num_data_type == "int":
+            plt.text(
+                x,
+                y,
+                "{0:d}".format(int(val)),
+                horizontalalignment="center",
+                verticalalignment="center",
+                color=color,
+                clip_on=True,
+                fontsize=8,
+            )
+        elif num_data_type == "str":
+            plt.text(
+                x,
+                y,
+                val,
+                horizontalalignment="center",
+                verticalalignment="center",
+                color=color,
+                clip_on=True,
+                fontsize=8,
+            )
 
-    def imshow(self, data,
-               left, right, ws,
-               bottom, top, hs,
-               num_data_type='float'):
+    def imshow(self, data, left, right, ws, bottom, top, hs, num_data_type="float"):
         wm = (right - left) / data.shape[1]
         hm = (top - bottom) / data.shape[0]
         hwm = wm / 2.0
         hhm = hm / 2.0
         hws = ws / 2.0
         hhs = hs / 2.0
-        width_range = np.linspace(
-            bottom - hhs + hhm, top + hhs - hhm, data.shape[0])
-        height_range = np.linspace(
-            left - hws + hwm, right + hws - hwm, data.shape[1])
-        plt.imshow(data,
-                   extent=(left - hws, right + hws, bottom - hhs, top + hhs),
-                   aspect='auto', origin='lower')
-        ys, xs = np.meshgrid(width_range, height_range, indexing='ij')
+        width_range = np.linspace(bottom - hhs + hhm, top + hhs - hhm, data.shape[0])
+        height_range = np.linspace(left - hws + hwm, right + hws - hwm, data.shape[1])
+        plt.imshow(
+            data,
+            extent=(left - hws, right + hws, bottom - hhs, top + hhs),
+            aspect="auto",
+            origin="lower",
+        )
+        ys, xs = np.meshgrid(width_range, height_range, indexing="ij")
         for (x, y, val) in zip(xs.flatten(), ys.flatten(), data.flatten()):
             self.plot_text(x, y, val, num_data_type)
         plt.colorbar()
@@ -105,7 +114,7 @@ class Plotter(object):
         plt.title(title)
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
-        plt.legend(loc='upper left', fontsize=6, labelspacing=0)
+        plt.legend(loc="upper left", fontsize=6, labelspacing=0)
 
     def show(self):
         if self.plot_flag:
