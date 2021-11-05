96d6a4908411fe48601454fd081087e597605da9
Fumiya Watanabe <rej55.g@gmail.com>
2021-11-05 13:39:14 +0900

    Update common/util/autoware_utils/include/autoware_utils/trajectory/convert.hpp
    Co-authored-by: Kenji Miyake <31987104+kenji-miyake@users.noreply.github.com>

--- common/util/autoware_utils/include/autoware_utils/trajectory/convert.hpp
+++ common/util/autoware_utils/include/autoware_utils/trajectory/convert.hpp
@@ -34,7 +34,7 @@ using TrajectoryPointArray = std::vector<autoware_auto_planning_msgs::msg::Traje
 // convertToTrajectoryByClipping() just clips TrajectoryPointArray up to the capacity of Trajectory.
 // Therefore, the error handling out of this function is necessary if the size of
 // TrajectoryPointArray greater than the capacity.
-// (TODO) Decide how handle the situation that we need to use the trajectory with size of points
+// TODO(Tier IV): Decide how to handle the situation that we need to use the trajectory with the size of points
 // larger than the capacity.
 Trajectory convertToTrajectoryByClipping(const TrajectoryPointArray & trajectory)
 {
