19112d2d4e504a5b16f625ff762de936bac90197
taichiH <azumade.30@gmail.com>
2021-11-30 15:40:11 +0900

    fix include

--- perception/object_segmentation/elevation_map_loader/include/elevation_map_loader/elevation_map_loader_node.hpp
+++ perception/object_segmentation/elevation_map_loader/include/elevation_map_loader/elevation_map_loader_node.hpp
@@ -16,7 +16,6 @@
 #define MAP_LOADER__ELEVATION_MAP_LOADER_NODE_HPP_
 
 #include <autoware_utils/geometry/boost_geometry.hpp>
-#include "autoware_external_api_msgs/msg/map_hash.hpp"
 #include <filters/filter_chain.hpp>
 #include <grid_map_core/GridMap.hpp>
 #include <grid_map_pcl/GridMapPclLoader.hpp>
@@ -26,6 +25,7 @@
 #include <nlohmann/json.hpp>
 #include <rclcpp/rclcpp.hpp>
 
+#include "autoware_external_api_msgs/msg/map_hash.hpp"
 #include <autoware_auto_mapping_msgs/msg/had_map_bin.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
 
@@ -33,6 +33,7 @@
 #include <pcl/point_types.h>
 
 #include <filesystem>
+#include <memory>
 #include <string>
 #include <vector>
 
